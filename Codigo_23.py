8c1f5515ded9888da4fe9673ab9cdaafacfc9a8d
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-10 13:52:50 -0300

    Update Codigo_23.py

---
+++ Codigo_23.py
@@ -0,0 +1,18 @@
+'''
+Formatação básica de strings
+
+* s - string
+* d - int
+* f - float
+* .<número de dígitos>f - float com n dígitos após a vírgula
+* x ou X - hexadecimal
+* (Caractere)(><^)(quantidade)
+* > - Esquerda
+* < - Direita
+* ^ - Centro
+* Sinal: - ou +
+* Ex: 0 >- 100,.1f
+* Conversion flags: !r, !s, !a
+
+'''
+
