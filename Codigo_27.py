c40b659471c7108c6760251d63116b228e74faeb
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-14 13:05:11 -0300

    Update Codigo_27.py

---
+++ Codigo_27.py
@@ -0,0 +1,19 @@
+'''
+CONSTANTE = "Variáveis" que não vão mudar
+Muitas condições no mesmo if (ruim)
+    <- Contagem de complexidade (ruim)
+    
+* Obs: As variáveis que não mudam, são escritas em maiúsculo
+
+'''
+
+import os
+
+os.system('color 1f')
+
+velocidade = 61 # Velocidade atual do carro
+local_carro = 100 # Local em que o carro está na estrada
+
+RADAR_1 = 60 # Velocidade máxima do radar 1
+LOCAL_1 = 100 # Local onde o radar 1 está
+RADAR_RANGE = 1 # A distância onde o radar pega
