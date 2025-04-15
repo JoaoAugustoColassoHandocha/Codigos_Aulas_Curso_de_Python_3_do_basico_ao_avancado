355513dad4a353137097e2227d3a5af5980005d2
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-15 12:41:44 -0300

    Update Codigo_28.py

---
+++ Codigo_28.py
@@ -0,0 +1,25 @@
+'''
+Flag (Bandeira) - Marcar um local
+None = Não Valor
+is e is not = É ou não é (tipo, valor, identidade)
+id = Identidade (local na memória)
+
+condicao = False
+
+if condicao:
+    print('Verdadeiro')
+else:
+    print('Falso')
+
+'''
+
+import os
+
+os.system('color 1f')
+
+v1 = 'a'
+
+print('\n' + id(v1) + '\n')
+
+os.system('pause')
+os.system('cls')
