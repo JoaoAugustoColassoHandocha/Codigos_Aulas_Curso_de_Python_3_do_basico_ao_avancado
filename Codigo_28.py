23c2843efd4084ad2abec1bd7c69b59e8ca0c190
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-15 13:06:10 -0300

    Update Codigo_28.py

--- Codigo_28.py
+++ Codigo_28.py
@@ -4,13 +4,6 @@ None = Não Valor
 is e is not = É ou não é (tipo, valor, identidade)
 id = Identidade (local na memória)
 
-condicao = False
-
-if condicao:
-    print('Verdadeiro')
-else:
-    print('Falso')
-
 '''
 
 import os
@@ -19,7 +12,38 @@ os.system('color 1f')
 
 v1 = 'a'
 
-print('\n' + id(v1) + '\n')
+print(id(v1))
+
+os.system('pause')
+os.system('cls')
+
+condicao = False
+passou_no_if = None
+
+if condicao == True:
+    
+    passou_no_if = True
+    print('\nVerdadeiro\n')
+    
+elif condicao == False:
+    
+    print('\nFalso\n')
+    
+else:
+    
+    print('\nErro no processamento!\n')
+    
+if passou_no_if is not None:
+    
+    print('Passou no if\n')
+    
+elif passou_no_if is None:
+    
+    print('Não passou no if\n')
+    
+else:
+    
+    print('Erro no processamento!\n')
 
 os.system('pause')
 os.system('cls')
