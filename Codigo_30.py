8408bd91303dcb6b5cd91dd0401961a6df7e7d47
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-23 13:07:43 -0300

    Update Codigo_30.py

--- Codigo_30.py
+++ Codigo_30.py
@@ -5,3 +5,26 @@ Imutáveis que vimos: str, int, float, bool
 
 '''
 
+import os
+
+os.system('color 1f')
+
+string = 'joão augusto'
+
+string_2 = string
+
+string_3 = f'{string[:3]}0{string[4:]}'
+
+print(f'\n{string}')
+
+print(f'\n{string_2}')
+
+print(f'\n{string_3}\n')
+
+os.system('pause')
+os.system('cls')
+
+print(f'\n{string.capitalize()}\n')
+
+os.system('pause')
+os.system('cls')
