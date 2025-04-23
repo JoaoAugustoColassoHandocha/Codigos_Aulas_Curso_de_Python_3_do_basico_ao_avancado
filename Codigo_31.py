b3293e338986440f728e2fa2a2bb468b59e54a38
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-23 13:36:26 -0300

    Update Codigo_31.py

--- Codigo_31.py
+++ Codigo_31.py
@@ -5,3 +5,29 @@ Executa uma ação enquanto uma condição for verdadeira.
 
 '''
 
+import os
+
+os.system('color 1f')
+
+cond = True
+
+while cond:
+    
+    name = input('\nQual o seu nome: ')
+    
+    os.system('cls')
+    
+    if name == 'Joao':
+             
+        cond = False
+        
+    else:
+        
+        print(f'\nSeu nome é {name} e está errado\n')
+        os.system('pause')
+        os.system('cls')
+        
+print(f'\nSeu nome é {name} e está certo\n')
+
+os.system('pause')
+os.system('cls')
