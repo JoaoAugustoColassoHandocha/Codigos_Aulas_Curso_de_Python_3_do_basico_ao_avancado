9fc8957caad0bf49c12b937edca7af8dcb3c1bbe
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-28 16:26:41 -0300

    Update Codigo_37.py

--- Codigo_37.py
+++ Codigo_37.py
@@ -6,55 +6,134 @@ Calculadora com while
 .startwith - Começa com...
 .endswith - Termina com...
 
+except Exception as error  - apresenta o erro ocorrido. Ex:
+
+try:
+
+    ...
+    
+except Exception as error:
+
+    print(error)
+
 '''
 
 import os
 
 os.system('color 1f')
 
-cls = os.system('cls')
-pause = os.system('pause')
-
 try:
 
     while True:
         
         print('\n' + '=' * 10 + 'Calculadora' + '=' * 10)
+        print('\n[A] - Acessar')
         print('\n[S] - Sair')
         print('\n' + '=' * 31)
         
-        op = input('Escoha uma opção: ')
+        op = input('\nEscolha uma opção: ').lower()
+        
+        os.system('cls')
         
         try:
         
-            if op.lower():
+            if op == 'a':
                 
+                print('\n' + '=' * 10 + 'Operadores' + '=' * 10)
+                print('\n[1] - Soma')
+                print('\n[2] - Subtração')
+                print('\n[3] - Multiplicação')
+                print('\n[4] - Divisão')
+                print('\n' + '=' * 30)
                 
-            
-            elif op.lower() == s:
+                oper = int(input('\nEcolha um operador: '))
+                
+                os.system('cls')
+                
+                if oper == 1 or oper == 2 or oper == 3 or oper == 4:
+                                       
+                    n1 = float(input('\nDigite o primeiro número: '))
+                    n2 = float(input('\nDigite o segundo número:'))
+                
+                    os.system('cls')
+                            
+                    if oper == 1:
+                        
+                        res = n1 + n2
+                                    
+                        print(f'\nO resultado de {n1:.2f} + {n2:.2f} = {res:.2f}\n')
+                        os.system('pause')
+                        os.system('cls')
+                                    
+                    elif oper == 2:
+                        
+                        res = n1 - n2
+                                
+                        print(f'\nO resultado de {n1:.2f} - {n2:.2f} = {res:.2f}\n')
+                        os.system('pause')
+                        os.system('cls')
+                                
+                    elif oper == 3:
+                        
+                        res = n1 * n2        
+                                
+                        print(f'\nO resultado de {n1:.2f} X {n2:.2f} = {res:.2f}\n')
+                        os.system('pause')
+                        os.system('cls')
+                                
+                    elif oper == 4:
+                        
+                        res = n1 / n2
+                                
+                        print(f'\nO resultado de {n1:.2f} / {n2:.2f} = {res:.2f}\n')
+                        os.system('pause')
+                        os.system('cls')
+                                
+                    else:
+                        
+                        os.system('cls')
+                        print('\nErro na opção repassada!!!\n')
+                        print('Favor verificar solicitação ou entrar em contato com o suporte técnico!\n')
+                        os.system('pause')
+                        os.system('cls')
+                
+                else:
+                    
+                    os.system('cls')
+                    print('\nErro na opção repassada!!!\n')
+                    print('Favor verificar solicitação ou entrar em contato com o suporte técnico!\n')
+                    os.system('pause')
+                    os.system('cls')
+                
+                    
+            elif op == 's':
                 
                 print('\nSaindo...\n')
-                pause
-                cls
+                
+                os.system('pause')
+                os.system('cls')
                 break
             
             else:
                 
+                os.system('cls')
                 print('\nErro na opção repassada!!!\n')
                 print('Favor verificar solicitação ou entrar em contato com o suporte técnico!\n')
-                pause
-                cls
+                os.system('pause')
+                os.system('cls')
         
-        except:
+        except Exception as error:
             
-            print('\nErro no processamento do dado repassado!!!\n')
+            os.system('cls')
+            print(f'\nErro: {error}\n')
             print('Favor verificar solicitação ou entrar em contato com o suporte técnico!\n')
-            pause
-            cls
+            os.system('pause')
+            os.system('cls')
 
-except:
+except Exception as error:
     
-    print('\nErro na execução do programa!!!\n')
+    os.system('cls')
+    print(f'\nErro: {error}\n')
     print('Favor entrar em contato com o suporte técnico!\n')
-    pause
-    cls
+    os.system('pause')
+    os.system('cls')
