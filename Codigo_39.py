783eb776d666fb9e96f533fd5dfd0872f1023d9e
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-29 15:57:06 -0300

    Update Codigo_39.py

--- Codigo_39.py
+++ Codigo_39.py
@@ -1,11 +1,9 @@
 '''
-* \ - quebra a linha.
+.count(palavra) -  Método para buscar quantas vezes a palavra ou letra foi repassada na frase ou texto.
 
-* .count(palavra) -  Método para buscar quantas vezes a palavra ou letra foi repassada na frase ou texto.
+.upper() - Insere o texto ou palavra todo MAIÚSCULO.
 
-* .upper() - Insere o texto ou palavra todo MAIÚSCULO.
-
-* .lower() - Insere o texto ou palavra todo minúsculo.
+.lower() - Insere o texto ou palavra todo minúsculo.
 
 '''
 
@@ -13,11 +11,43 @@ import os
 
 os.system('color 1f')
 
+# \ - quebra de linha no código
 frase = 'O python é uma linguagem de programação'\
         'multiparadigma. '\
-        'Pyhton foi criado por Guido Van Rossum.'
+        'Python foi criado por Guido Van Rossum.'.upper()
         
-print(f'\n{frase.count('python')}\n')
+print(f'\nQuantas vezes apareceu a palavra "Python": {frase.count('PYTHON')}')
+print(f'\nQuantas vezes apareceu a letra "A": {frase.count('A')}\n')
+
+os.system('pause')
+os.system('cls')
+
+i = 0
+
+qtd_mais_vzs_letra = 0
+mais_vzs_letra = ''
+
+while i < len(frase):
+    
+    letra_atual = frase[i]
+    
+    if letra_atual == ' ':
+        
+        i += 1
+        
+        continue
+    
+    n_vez_letra = frase.count(letra_atual)
+    
+    if qtd_mais_vzs_letra < n_vez_letra:
+        
+        qtd_mais_vzs_letra = n_vez_letra
+        
+        mais_vzs_letra = letra_atual
+    
+    i += 1
+    
+print(f'\nA letra que mais apareceu foi "{mais_vzs_letra}", sendo {qtd_mais_vzs_letra} vezes.\n')
 
 os.system('pause')
 os.system('cls')
