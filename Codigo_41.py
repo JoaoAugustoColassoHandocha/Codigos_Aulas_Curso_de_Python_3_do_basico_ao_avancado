e0c59cd22528bd6ad61ab88d32520d9a82556257
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-30 15:38:33 -0300

    Update Codigo_41.py

--- Codigo_41.py
+++ Codigo_41.py
@@ -1,5 +1,58 @@
 '''
 For + Range
-range -> range(start, stop, step)
+range -> range(start - Início, stop - Final, step - Pular de quantos em quantos)
 
 '''
+
+import os
+
+os.system('color 1f')
+
+numeros1 = range(11)
+numeros2 = range (5, 11)
+numeros3 = range(0, 11, 2)
+numeros4 = range(0, -11, -2)
+
+print('\n')
+
+for valor1 in numeros1:
+    
+    print(valor1)
+
+print('\n')
+
+os.system('pause')
+os.system('cls')
+
+print('\n')
+
+for valor2 in numeros2:
+    
+    print(valor2)
+
+print('\n')
+
+os.system('pause')
+os.system('cls')
+
+print('\n')
+
+for valor3 in numeros3:
+    
+    print(valor3)
+
+print('\n')
+
+os.system('pause')
+os.system('cls')
+
+print('\n')
+
+for valor4 in numeros4:
+    
+    print(valor4)
+
+print('\n')
+
+os.system('pause')
+os.system('cls')
