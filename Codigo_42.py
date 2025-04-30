e8edae7f1a038b508af8401e9fd01e9703673101
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-04-30 16:14:46 -0300

    Update Codigo_42.py

--- Codigo_42.py
+++ Codigo_42.py
@@ -1,4 +1,24 @@
 '''
-
+* Iterável -> str, range, entre outros
+* Iterador -> Quem sabe entregar um valor por vez
+* next -> Me entregue o próximo valor
+* iter -> Me entregue seu iterador
 
 '''
+
+import os
+
+os.system('color 1f')
+
+numeros = range(0, 101, 8)
+
+print('\n')
+
+for numero in numeros:
+    
+    print(numero)
+    
+print('\n')
+
+os.system('pause')
+os.system('cls')
