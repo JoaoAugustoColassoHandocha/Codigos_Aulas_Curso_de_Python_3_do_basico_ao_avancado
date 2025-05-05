c8ddf709335e44744c3bfd04456104585117f2a4
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-05 13:32:02 -0300

    Update Codigo_44.py

--- Codigo_44.py
+++ Codigo_44.py
@@ -3,7 +3,7 @@ Faça um jogo para o usuário adivinhar qual a palavra secreta.
 
 * Você vai propor uma palavra secreta qualquer e vai dar a possibilidade para o usuário digitar apenas uma letra.
 
-* Qual o usuário digitar uma letra, você vai conferir se a letra digitada está na palavra secreta.
+* Quando o usuário digitar uma letra, você vai conferir se a letra digitada está na palavra secreta.
 
 * Se a letra digitada estiver na palavra secreta, exiba a letra
 
@@ -12,3 +12,22 @@ Faça um jogo para o usuário adivinhar qual a palavra secreta.
 Faça a contagem de tentativas do seu usuário.
 
 '''
+
+import os
+
+os.system('Color 1f')
+
+def main(op):
+    
+    print('\n' + '=' * 10 + 'PALAVRA SECRETA' + '=' * 10 + '\n')
+    print('[C] - Começar o Jogo')
+    print('[S] - Sair')
+    print('=' * 35)
+    
+    op.upper() = input('Opção: ')
+    
+    if op == 'C':
+        
+        
+    
+main()
