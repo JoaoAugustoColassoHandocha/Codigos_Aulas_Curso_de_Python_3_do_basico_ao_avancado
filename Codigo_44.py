d643131c1406c309824dfecdfe3e12fd5b8c2918
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-05 17:05:42 -0300

    Update Codigo_44.py

--- Codigo_44.py
+++ Codigo_44.py
@@ -14,6 +14,7 @@ Faça a contagem de tentativas do seu usuário.
 '''
 
 import os
+import random
 
 os.system('Color 1f')
 
@@ -22,12 +23,49 @@ def main(op):
     print('\n' + '=' * 10 + 'PALAVRA SECRETA' + '=' * 10 + '\n')
     print('[C] - Começar o Jogo')
     print('[S] - Sair')
-    print('=' * 35)
+    print('\n' + '=' * 35 + '\n')
     
-    op.upper() = input('Opção: ')
+    op = input('Opção: ')
     
-    if op == 'C':
+    os.system('cls')
         
+    
+    if op.upper() == 'C':
+             
+        jogo()
+        
+    elif op.upper() == 'S':
+        
+        print('\nSaindo...\n')
+        os.system('pause')
+        os.system('cls')
         
     
-main()
+def jogo():
+    
+    palavras_secretas = ['teste', 'banana', 'dia', 'noite', 'tarde', 'sol', 'lua', 'cafe', 'amor', 'pao', 'pai', 'mae', 'filho', 'filha', 'neto', 'neta', 'avo', 'tio', 'tia']
+    
+    palavra_secreta_sorteada = random.choice(palavras_secretas)
+    
+    tamanho_palavra = len(palavra_secreta_sorteada)
+    
+    esconde_palavra_secreta = ''
+    
+    i = 0
+        
+    for i in tamanho_palavra:
+            
+        esconde_palavra_secreta += '*'
+    
+    while True:
+        
+        print(f'\n{esconde_palavra_secreta}')
+        
+        letra_secreta = input('\nDigite a letra que faz parte da palavra secreta: ')
+        
+        os.system('cls')
+        
+        break
+        
+
+main(op = '')
