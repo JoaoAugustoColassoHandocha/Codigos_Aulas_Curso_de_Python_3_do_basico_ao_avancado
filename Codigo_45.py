614b1f9dfbd80863e6aa063dcebfe2f5e64b2855
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-06 16:33:57 -0300

    Update Codigo_45.py

--- Codigo_45.py
+++ Codigo_45.py
@@ -1,4 +1,35 @@
 '''
+Listas em Python
 
+Tipo list - Mutável
+
+Suporta vários valores de qualquer tipo
+
+Conhecimentos reutilizáveis - índices e fatiamento
+
+Métodos úteis: append, insert, pop, del, clear, extend, +
 
 '''
+
+import os
+
+os.system('color 1f')
+
+string = 'ABCDE'
+
+lista = [123, True, 'Joao', 1.2, []]
+
+print(f'\n{lista[0]}')
+print(f'\n{lista[1]}')
+print(f'\n{lista[2]}')
+print(f'\n{lista[3]}')
+print(f'\n{lista[4]}')
+
+print('\n' + '=' * 30 + '\n')
+
+lista[2] = 'Maria'
+
+print(f'{lista[2]}\n')
+
+os.system('pause')
+os.system('cls')
