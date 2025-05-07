67b4ff25bab712e854965ffb23763e7442a52508
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-07 15:59:34 -0300

    Update Codigo_46.py

---
+++ Codigo_46.py
@@ -0,0 +1,16 @@
+'''
+Listas em Python
+
+Tipo list - Mutável
+
+Suporta vários valores de qualquer tipo
+
+Conhecimentos reutilizáveis - índices e fatiamento
+
+Métodos úteis: append, insert, pop, del, clear, extend, +
+
+Create Read Update Delete
+
+Criar, lerm alterar , apagar = lista[i] (CRUD)
+
+'''
