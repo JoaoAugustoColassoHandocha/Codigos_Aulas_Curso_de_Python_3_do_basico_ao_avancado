b7a06629fc605b296935dd45248f92f83d2d45cd
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-08 10:44:29 -0300

    Update Codigo_47.py

---
+++ Codigo_47.py
@@ -0,0 +1,22 @@
+'''
+Conhecimentos reutilizáveis - índices e fatiamento
+
+Métodos úteis:
+
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - Apaga um índice
+    clear - Limpa a lista
+    extend - Estende a lista
+    + - Concatena listas
+
+CRUD:
+
+Create | Read | Update  | Delete
+Criar  | Ler  | Alterar | Apagar
+
+lista[i] (CRUD)
+
+'''
+
