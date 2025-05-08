0ed991a8e734274829047011c90712eaeb0e4903
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-08 15:05:52 -0300

    Update Codigo_48.py

--- Codigo_48.py
+++ Codigo_48.py
@@ -21,3 +21,23 @@ lista[i] (CRUD)
 lista.insert(índice, item)
 
 '''
+
+import os
+
+os.system('color 1f')
+
+# Repassando as listas A e B, concatenando em uma lista C.
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+print('\n' + '=' * 30 + '\n')
+print(f'{lista_c}\n')
+print('=' * 30 + '\n')
+
+# Concatenando as listas A e B na própria lista A.
+lista_a.extend(lista_b)
+print(f'{lista_a}\n')
+print('=' * 30 + '\n')
+
+os.system('pause')
+os.system('cls')
