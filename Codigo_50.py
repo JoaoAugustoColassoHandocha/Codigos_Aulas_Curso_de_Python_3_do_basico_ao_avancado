6a78d477536b23cdfcfa6ae9182f834475691b98
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-09 13:20:49 -0300

    Codigo_50.py

---
+++ Codigo_50.py
@@ -0,0 +1,22 @@
+'''
+for in com listas
+
+'''
+
+import os
+
+os.system('color 1f')
+
+lista = ['Maria', 'Helena', 'Joao']
+
+print('\n' + '=' * 30)
+
+for nome in lista:
+    
+    print(f'\n= {nome} =')
+    print('\n' + '=' * 30)
+
+print('\n')
+   
+os.system('pause')
+os.system('cls')
