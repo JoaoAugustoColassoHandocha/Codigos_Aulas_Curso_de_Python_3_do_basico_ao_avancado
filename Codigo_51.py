826ee3486167668d8278970d11ee1fd908c2c5cf
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-09 13:34:43 -0300

    Update Codigo_51.py

--- Codigo_51.py
+++ Codigo_51.py
@@ -11,5 +11,18 @@ os.system('color 1f')
 
 lista = ['Maria', 'Helena', 'Joao']
 
+lista.append('Luiz')
+
+indices = range(len(lista))
+
+print('\n' + '=' * 30)
+
+for indice in indices:
+    
+    print(f'\n{indice} - {lista[indice]}')
+    print('\n' + '=' * 30)
+    
+print('\n')
+    
 os.system('pause')
 os.system('cls')
