4fc8086d0a6ca61a3d4480523a5a5cbbd3a357df
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-13 13:21:06 -0300

    Update Codigo_53.py

--- Codigo_53.py
+++ Codigo_53.py
@@ -1,5 +1,13 @@
 '''
-Tipo tupla - Um alista imutável
+Tipo tupla - Uma lista imutável
+
+* Não é possível realizar a mudança de algum dado da lista
+
+"nomes[1] = mudar" - Apresentará erro TypeError: 'tuple' object does not support item assignment.
+
+tuple() - Função para transformar uma lista mutável em tupla.
+
+list() - Função para transformar uma tupla em lista mutável.
 
 '''
 
@@ -7,9 +15,14 @@ import os
 
 os.system('color 1f')
 
-_, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+nomes = ('Maria', 'Helena', 'Luiz')
 
-print(f'\n')
+print(f'\n{nomes}\n')
+print(f'{nomes[1]}\n')
+
+nome = ['Maria', 'Helena', 'Luiz']
+
+print(f'{tuple(nome)}\n')
 
 os.system('pause')
 os.system('cls')
