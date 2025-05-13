cd35b144ed8b8f90420e3893bf49870e4d1e5953
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-13 13:44:33 -0300

    Update Codigo_54.py

--- Codigo_54.py
+++ Codigo_54.py
@@ -1,13 +1,33 @@
 '''
 Enumerate - Enumera iteráveis (índices)
 
+enumerate()
+
+\t - TAB
+
 '''
 
 import os
 
 os.system('color 1f')
 
+lista = ['Maria', 'Helena', 'Luiz']
 
+lista.append('João')
+
+print('\n')
+
+for indice, nome in enumerate(lista):
+
+    print(indice, nome)
+    
+print('\n')
+    
+# Outra forma de fazer
+
+lista_enumerada = list(enumerate(lista))
+
+print(f'{lista_enumerada}\n')
 
 os.system('pause')
 os.system('cls')
