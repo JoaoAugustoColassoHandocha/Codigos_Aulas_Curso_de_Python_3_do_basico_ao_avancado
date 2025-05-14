53fac25f7a5f5b32d5cea16e5acc033d16447a98
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-14 14:39:41 -0300

    Update Codigo_55.py

--- Codigo_55.py
+++ Codigo_55.py
@@ -13,68 +13,97 @@ os.system('color 1f')
 
 lista = []
 
-def main():
+try:
+    
+    def main():
 
-    print('\n' + '-' * 5 + 'LISTA DE COMPRA' + '-' * 5 + '\n')
-    print('[1] - Acessar o sistema')
-    print('[2] - Sair')
-    print('\n' + '-' * 25 + '\n')
+        print('\n' + '-' * 5 + 'LISTA DE COMPRA' + '-' * 5 + '\n')
+        print('[A] - Acessar o sistema')
+        print('[S] - Sair')
+        print('\n' + '-' * 25 + '\n')
+            
+        op = input('Escolha uma opção: ')
+            
+        os.system('cls')
+            
+        if op == '' or op == ' ':
+            
+            print('\nFavor repassar a opção desejada!!!\n')
+            os.system('pause')
+            os.system('cls')
+            main()
         
-    op = input('Escolha uma opção: ')
+        elif op.upper() == 'A':
+            
+            senha = getpass.getpass('\nFavor repassar a senha de acesso: ')
+            
+            os.system('cls')
+            
+            while True:
+            
+                if senha == 'AcessoLista':
+                    
+                    lista_compra()
+                    break
+                
+                elif senha == '' or senha == ' ':
+                    
+                    print('\nFavor repassar a senha solicitada!!!\n')
+                    os.system('pause')
+                    os.system('cls')
+                    main()
+                    break
+                
+                elif senha != 'AcessoLista':
+                    
+                    print('\nSENHA ERRADA!!!')
+                    print('\nFavor inserir senha correta!\n')
+                    os.system('pause')
+                    os.system('cls')
+                    main()
+                    break
+                
+                else: 
+                    
+                    print('\nERRO!!!')
+                    print('\nFavor entrar em contato com o suporte do sistema.')
+                    os.system('pause')
+                    os.system('cls')
+                    main()
+                    break   
+                                        
+        elif op.upper() == 'S':
+            
+            print('\nSaindo...\n')
+            os.system('pause')
+            os.system('cls')
+            
+        elif op.upper() != 'A' or op.upper() != 'S':
+            
+            print('\nOPÇÃO ERRADA!!!')
+            print('\nFavor inserir opção certa.\n')
+            os.system('pause')
+            os.system('cls')
+            main()
+            
+        else:
+            
+            print('\nERRO!!!')
+            print('\nFavor entrar em contato com o suporte do sistema.')
+            os.system('pause')
+            os.system('cls')
+            
+    def lista_compra():
         
-    os.system('cls')
-    
-    if op != 1 or op != 2:
-        
-        print('\nOPÇÃO ERRADA!!!')
-        print('\nFavor inserir opção certa.\n')
+        print('\nteste\n')
         os.system('pause')
         os.system('cls')
-        main()
+        
+except:
     
-    elif op == 1:
-        
-        senha = getpass.getpass('Favor repassar a senha de acesso: ')
-        
-        os.system('cls')
-        
-        while True:
-        
-            if senha == 'AcessoLista':
-                
-                lista_compra()
-                break
-                
-            elif senha != 'AcessoLista':
-                
-                print('\nSENHA ERRADA!!!')
-                print('\nFavor inserir senha correta!')
-                os.system('pause')
-                os.system('cls')
-                main()
-                        
-    elif op == 2:
-        
-        print('\nSaindo...\n')
-        os.system('pause')
-        os.system('cls')
-        os.system('exit')
-        
-    else:
-        
-        print('\nERRO!!!')
-        print('\nFavor entrar em contato com o suporte do sistema.')
-        os.system('pause')
-        os.system('cls')
-        os.system('exit')
-        
-def lista_compra():
-    
-    print('\nteste\n')
+    print('\nERRO!!!')
+    print('\nFavor entrar em contato com o suporte do sistema.')
     os.system('pause')
     os.system('cls')
-    
+        
 main()
-
-os.system('pause')
-os.system('cls')
