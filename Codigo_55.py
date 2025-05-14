9d262ddb488986d625894a55b98129872cad984e
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-14 13:08:37 -0300

    Update Codigo_55.py

--- Codigo_55.py
+++ Codigo_55.py
@@ -11,7 +11,14 @@ import os
 
 os.system('color 1f')
 
+def main():
 
+    print('\n' + '-' * 5 + 'LISTA DE COMPRA' + '-' * 5 + '\n')
+    print('[1] - Cadastrar produto')
+    print('[2] - Mostrar lista')
+    print('[3] - Apagar produto')
+    print('[4] - Sair')
+    print('\n' + '-' * 25)
 
 os.system('pause')
 os.system('cls')
