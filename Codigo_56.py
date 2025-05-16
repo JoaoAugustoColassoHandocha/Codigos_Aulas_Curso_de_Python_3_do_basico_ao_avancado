2c5c9a150911f060a2f786a42442bc022dfe0e93
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-16 10:05:53 -0300

    Update Codigo_56.py

--- Codigo_56.py
+++ Codigo_56.py
@@ -16,15 +16,15 @@ numero_3 = numero_1 + numero_2
 
 # Apresenta o problema de imprecisão, imprimindo o valor 0.7999999999...
 print('\n' + '+' * 10 + '\n')
-print(numero_3)
+print(f'Sem Formatação: {numero_3}')
 print('\n' + '+' * 10 + '\n')
 
 # Primeira forma de contornar, imprimindo 0.80
-print(f'{numero_3:.2f}')
+print(f'Utilizando o Format: {numero_3:.2f}')
 print('\n' + '+' * 10 + '\n')
 
 # Segunda forma de contornar, imprimindo 0.8
-print(round(numero_3, 1))
+print(f'Utilizando o Round: {round(numero_3, 1)}')
 print('\n' + '+' * 10 + '\n')
 
 # Terceira forma de contornar, imprimindo
@@ -34,7 +34,7 @@ numero_2 = decimal.Decimal('0.7')
 
 numero_3 = numero_1 + numero_2
 
-print(numero_3)
+print(f'Utilizando a biblioteca decimal: {numero_3}')
 print('\n' + '+' * 10 + '\n')
 
 os.system('pause')
