a519f5b98452e2d37eee39b2ace1d09b9debab07
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-16 09:46:11 -0300

    Update Codigo_56.py

--- Codigo_56.py
+++ Codigo_56.py
@@ -4,3 +4,17 @@ Imprecisão de ponto flutuante
 Double-precision floating-point format IEEE 754
 
 '''
+
+import os
+
+os.system('color 1f')
+
+numero_1 = 0.1
+numero_2 = 0.7
+
+numero_3 = numero_1 + numero_2
+
+print(f'\n{numero_3}\n')
+
+os.system('pause')
+os.system('cls')
