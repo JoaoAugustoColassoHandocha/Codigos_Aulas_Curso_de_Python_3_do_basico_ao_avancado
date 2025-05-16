c7237b00d116ac8d2f42e9d360c70c5419d270c5
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-16 10:32:46 -0300

    Update Codigo_57.py

--- Codigo_57.py
+++ Codigo_57.py
@@ -5,6 +5,8 @@ split - divide uma string
 
 join - une uma string
 
+strip() - Corta os espaços do início e final da informação imprimida
+
 '''
 
 import os
@@ -13,5 +15,29 @@ os.system('color 1f')
 
 frase = 'Olha só que, coisa interessante'
 
+#######################
+
+lista_palavras_1 = frase.split()
+
+print(f'\n{lista_palavras_1}\n')
+
+#######################
+
+lista_palavras_2 = frase.split(',')
+
+print(f'{lista_palavras_2}\n')
+
+#######################
+
+lista_palavras_3 = frase.split(', ')
+
+for i, frase in enumerate(lista_palavras_3):
+    
+    print(lista_palavras_3[i])
+    
+print('\n')
+
+#######################
+    
 os.system('pause')
 os.system('cls')
