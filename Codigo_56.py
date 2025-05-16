b4501f1d5f096097a2d80ef2b2be6d6c8df6fc5f
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-16 09:55:14 -0300

    Update Codigo_56.py

--- Codigo_56.py
+++ Codigo_56.py
@@ -14,7 +14,16 @@ numero_2 = 0.7
 
 numero_3 = numero_1 + numero_2
 
-print(f'\n{numero_3}\n')
+# Apresenta o problema de imprecisão, imprimindo o valor 0.7999999999...
+print('\n' + '+' * 10 + '\n')
+print(numero_3)
+print('\n' + '+' * 10 + '\n')
+
+# Primeira forma de contornar, imprimindo 0.80
+print(f'{numero_3:.2f}')
+print('\n' + '+' * 10 + '\n')
+
+# Segunda forma de contornar
 
 os.system('pause')
 os.system('cls')
