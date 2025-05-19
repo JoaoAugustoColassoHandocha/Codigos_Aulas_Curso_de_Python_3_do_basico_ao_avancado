cf235345ae669e310f3df06323492c150efcdbc8
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-19 13:31:16 -0300

    Update Codigo_58.py

--- Codigo_58.py
+++ Codigo_58.py
@@ -2,3 +2,24 @@
 Lista de listas e seus índices
 
 '''
+
+import os
+
+os.system('color 1f')
+
+salas = [
+    #   0        1
+    ['Maria', 'Helena', ], # 0
+    
+    #   0
+    ['Elaine', ], # 1
+    
+    #  0       1         2
+    ['Luiz', 'João', 'Eduarda', ], # 2
+    
+]
+
+print(f'\n{salas}\n')
+
+os.system('pause')
+os.system('cls')
