90dbcefa98261fbf315c995ec7107fef6b490534
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-19 14:27:01 -0300

    Update Codigo_59.py

--- Codigo_59.py
+++ Codigo_59.py
@@ -3,3 +3,17 @@ Desempacotamento em chamadas de métodos e funções
 
 '''
 
+import os
+
+os.system('color 1f')
+
+string = 'ABCD'
+lista = ['Maria', 'Helena', 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+
+a, b, c = lista
+
+print(f'\n{a, c}\n')
+
+os.system('pause')
+os.system('cls')
