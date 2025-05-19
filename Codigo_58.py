7d39231d34d83c50ba8add78de85442dcfd86db2
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-19 13:37:11 -0300

    Codigo_58.py

--- Codigo_58.py
+++ Codigo_58.py
@@ -14,14 +14,16 @@ salas = [
     #   0
     ['Elaine', ], # 1
     
-    #  0       1         2
-    ['Luiz', 'João', 'Eduarda', ], # 2
+    #  0       1         2                3
+    ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)], # 2
     
 ]
 
 print(f'\n{salas}\n')
 
 print(f'{salas[2][1]}\n')
+
+print(f'{salas[2][3][3]}\n')
 
 os.system('pause')
 os.system('cls')
