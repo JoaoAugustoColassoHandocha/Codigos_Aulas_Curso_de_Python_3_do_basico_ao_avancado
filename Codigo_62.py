f3bc06901a59f08912faabb4f63c7019d672023d
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-23 10:25:12 -0300

    Update Codigo_62.py

--- Codigo_62.py
+++ Codigo_62.py
@@ -42,6 +42,19 @@ while True:
         try:
             
             nove_digitos = cpf[:9]
+            contador_regressivo = 10
+            resultado = 0
+            
+            for digito in nove_digitos:
+                
+                resultado += int(digito) * contador_regressivo
+                contador_regressivo -= 1
+            
+            digito = (resultado * 10) % 11
+            
+            digito = digito if digito <= 9 else 0
+            
+            
             
             
         except ValueError:
