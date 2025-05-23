c03a91a59623a7611d07944f4190f860da4ac49d
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-23 14:12:48 -0300

    Update Codigo_64.py

--- Codigo_64.py
+++ Codigo_64.py
@@ -43,26 +43,40 @@ while True:
         try:
             
             nove_digitos = cpf[:9]
-            contador_regressivo = 10
-            resultado = 0
+            contador_regressivo_1 = 10
+            resultado_1 = 0
             
             for digito in nove_digitos:
                 
-                resultado += int(digito) * contador_regressivo
-                contador_regressivo -= 1
+                resultado_1 += int(digito) * contador_regressivo_1
+                contador_regressivo_1 -= 1
             
-            digito = (resultado * 10) % 11
+            digito_1 = (resultado_1 * 10) % 11
             
-            digito = digito if digito <= 9 else 0
+            digito_1 = digito_1 if digito_1 <= 9 else 0
             
-            if str(digito) == cpf[9]:
+            dez_digitos = cpf[:10]
+            contador_regressivo_2 = 11
+            resultado_2 = 0
+            
+            for digito in dez_digitos:
+                
+                resultado_2 += int(digito) * contador_regressivo_2
+                contador_regressivo_2 -= 1
+                
+            digito_2 = (resultado_2 * 10) % 11
+            
+            digito_2 = digito_2 if digito_2 <= 9 else 0
+            
+            
+            if str(digito_1) == cpf[9] and str(digito_2) == cpf[10]:
                 
                 print('\nO primeiro dígito do CPF é verdadeiro!\n')
                 os.system('pause')
                 os.system('cls')
                 break
             
-            elif str(digito) != cpf[9]:
+            elif str(digito_1) != cpf[9] or str(digito_2) != cpf[10]:
                 
                 print('\nO primeiro dígito do CPF é falso!\n')
                 os.system('pause')
@@ -74,7 +88,7 @@ while True:
                 os.system('pause')
                 os.system('cls')
                 break
-        
+            
         except ValueError:
             
             print('\nCPF inválido!\n')
