89bea2716df4b4fa6423604292a12dd96e529f7f
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-26 13:35:08 -0300

    Update Codigo_65.py

--- Codigo_65.py
+++ Codigo_65.py
@@ -50,14 +50,14 @@ while True:
             
             if str(digito_1) == corrigir_cpf[9] and str(digito_2) == corrigir_cpf[10]:
                 
-                print('\nO primeiro dígito do CPF é verdadeiro!\n')
+                print('\nO CPF é verdadeiro!\n')
                 os.system('pause')
                 os.system('cls')
                 break
             
             elif str(digito_1) != corrigir_cpf[9] or str(digito_2) != corrigir_cpf[10]:
                 
-                print('\nO primeiro dígito do CPF é falso!\n')
+                print('\nO CPF é falso!\n')
                 os.system('pause')
                 os.system('cls')
             
