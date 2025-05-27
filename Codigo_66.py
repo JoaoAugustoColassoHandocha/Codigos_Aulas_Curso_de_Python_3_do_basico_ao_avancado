1369b017fc3c9e354ee3e045afb015ab06b8d015
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-27 13:41:08 -0300

    Update Codigo_66.py

--- Codigo_66.py
+++ Codigo_66.py
@@ -7,73 +7,75 @@ import os, re, random
 
 os.system('color 1f')
 
-while True:
-    
-    num_nove_cpf = ''
-    
-    cpf_form = ''
-    
-    for i in range(9):
-        
-        num_nove_cpf += str(random.randint(0, 9))
+for a in range(10):
 
-    corrigir_cpf = re.sub(r'[^0-9]', '', num_nove_cpf)
-    
-    prim_char_ent_cpf_rep = corrigir_cpf == corrigir_cpf[0] * len(cpf_form)
-    
-          
-    try:
+    while True:
+        
+        num_nove_cpf = ''
+        
+        cpf_form = ''
+        
+        for i in range(9):
             
-        nove_digitos = corrigir_cpf[:9]
-        contador_regressivo_1 = 10
-        resultado_1 = 0
+            num_nove_cpf += str(random.randint(0, 9))
+
+        corrigir_cpf = re.sub(r'[^0-9]', '', num_nove_cpf)
+        
+        prim_char_ent_cpf_rep = corrigir_cpf == corrigir_cpf[0] * len(cpf_form)
+        
             
-        for digito in nove_digitos:
+        try:
                 
-            resultado_1 += int(digito) * contador_regressivo_1
-            contador_regressivo_1 -= 1
-            
-        digito_1 = (resultado_1 * 10) % 11
-            
-        digito_1 = digito_1 if digito_1 <= 9 else 0
-            
-        dez_digitos = corrigir_cpf[:10]
-        contador_regressivo_2 = 11
-        resultado_2 = 0
-            
-        for digito in dez_digitos:
+            nove_digitos = corrigir_cpf[:9]
+            contador_regressivo_1 = 10
+            resultado_1 = 0
                 
-            resultado_2 += int(digito) * contador_regressivo_2
-            contador_regressivo_2 -= 1
+            for digito in nove_digitos:
+                    
+                resultado_1 += int(digito) * contador_regressivo_1
+                contador_regressivo_1 -= 1
                 
-        digito_2 = (resultado_2 * 10) % 11
-            
-        digito_2 = digito_2 if digito_2 <= 9 else 0
-            
-        cpf_form = num_nove_cpf + str(digito_1) + str(digito_2)          
-            
-        if str(digito_1) == cpf_form[9] and str(digito_2) == cpf_form[10]:
+            digito_1 = (resultado_1 * 10) % 11
                 
-            print(f'\nO CPF {cpf_form} é verdadeiro!\n')
+            digito_1 = digito_1 if digito_1 <= 9 else 0
+                
+            dez_digitos = corrigir_cpf[:10]
+            contador_regressivo_2 = 11
+            resultado_2 = 0
+                
+            for digito in dez_digitos:
+                    
+                resultado_2 += int(digito) * contador_regressivo_2
+                contador_regressivo_2 -= 1
+                    
+            digito_2 = (resultado_2 * 10) % 11
+                
+            digito_2 = digito_2 if digito_2 <= 9 else 0
+                
+            cpf_form = num_nove_cpf + str(digito_1) + str(digito_2)          
+                
+            if str(digito_1) == cpf_form[9] and str(digito_2) == cpf_form[10]:
+                    
+                print(f'\nO CPF {cpf_form} é verdadeiro!\n')
+                os.system('pause')
+                os.system('cls')
+                break
+                
+            elif str(digito_1) != cpf_form[9] or str(digito_2)!= cpf_form[10]:
+                    
+                print(f'\nO CPF {cpf_form} é falso!\n')
+                os.system('pause')
+                os.system('cls')
+                
+            else:
+                    
+                print('\nErro!!! Favor entrar em contato com o suporte!\n')
+                os.system('pause')
+                os.system('cls')
+                break
+                
+        except ValueError:
+                
+            print('\nCPF inválido!\n')
             os.system('pause')
             os.system('cls')
-            break
-            
-        elif str(digito_1) != cpf_form[9] or str(digito_2)!= cpf_form[10]:
-                
-            print(f'\nO CPF {cpf_form} é falso!\n')
-            os.system('pause')
-            os.system('cls')
-            
-        else:
-                
-            print('\nErro!!! Favor entrar em contato com o suporte!\n')
-            os.system('pause')
-            os.system('cls')
-            break
-            
-    except ValueError:
-            
-        print('\nCPF inválido!\n')
-        os.system('pause')
-        os.system('cls')
