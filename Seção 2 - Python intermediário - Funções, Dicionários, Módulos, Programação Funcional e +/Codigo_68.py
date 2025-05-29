990c9461a6448450057180ae7117caf045314be3
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-29 13:24:12 -0300

    Update Codigo_68.py

--- Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_68.py
+++ Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_68.py
@@ -21,3 +21,18 @@ print('\n')
 os.system('pause')
 os.system('cls')
 gc.collect()
+
+###############################
+
+def subtracao(x, y, z):
+    
+    print(f'{x = }, {y = } e {z = } | x - y - z = ', x - y - z)
+    
+print('\n')
+soma(1, 2 , 3)
+soma(y = 2, z = 3, x = 1)
+print('\n')
+
+os.system('pause')
+os.system('cls')
+gc.collect()
