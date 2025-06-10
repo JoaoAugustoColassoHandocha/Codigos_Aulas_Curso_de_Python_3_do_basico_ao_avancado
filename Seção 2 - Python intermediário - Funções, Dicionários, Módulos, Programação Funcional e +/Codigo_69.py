d0a67df28164149f1f0a6ca683c1105dcc21a234
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-06-10 12:13:45 -0300

    Update Codigo_69.py

--- Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_69.py
+++ Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_69.py
@@ -3,6 +3,8 @@ Valores padrão para parâmetros
 Ao definir uma função, os parâmetros podem ter valores padrão.
 Caso o valor não seja enviado para o parâmetro, o valor padrão será usado.
 
+Refatorar - Editar o seu código.
+
 '''
 
 import os
