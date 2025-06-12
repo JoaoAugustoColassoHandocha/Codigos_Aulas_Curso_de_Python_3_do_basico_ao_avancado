64f9c927c07d95bc4f0778bd5bd69a09b019c9e2
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-06-12 13:21:16 -0300

    Update Codigo_70.py

--- Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_70.py
+++ Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_70.py
@@ -6,3 +6,12 @@ O escopo global é onde todo o código é alcançavel.
 o escopo local é onde apenas nomes do mesmo local podem ser alcançados.
 
 '''
+
+import os
+
+os.system('color 1f')
+
+
+
+os.system('pause')
+os.system('cls')
