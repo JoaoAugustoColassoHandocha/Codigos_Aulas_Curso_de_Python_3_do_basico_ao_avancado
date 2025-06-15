7ee22aaf970d7d427303913dd84b99595acf01ad
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2025-06-15 19:58:34 -0300

    Update Codigo_70.py

--- Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_70.py
+++ Seção 2 - Python intermediário - Funções, Dicionários, Módulos, Programação Funcional e +/Codigo_70.py
@@ -3,7 +3,7 @@ Escopo de funções em Python
 Escopo significa o local onde aquele código pode atingir.
 Existe o escopo global e local.
 O escopo global é onde todo o código é alcançavel.
-o escopo local é onde apenas nomes do mesmo local podem ser alcançados.
+O escopo local é onde apenas nomes do mesmo local podem ser alcançados.
 
 '''
 
