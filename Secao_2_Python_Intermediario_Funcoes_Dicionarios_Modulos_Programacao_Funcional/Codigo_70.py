cc79992bac76b9edf193bce3e4dc277716540992
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-06 00:16:22 -0300

    Update Codigo_70.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_70.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_70.py
@@ -7,6 +7,8 @@ O escopo local é onde apenas nomes do mesmo local podem ser alcançados.
 
 Não temos acesso a nomes de escopos internos nos escopos externos.
 
+A palavra global faz uma váriavel do escopo externo ser a mesma no escopo interno.
+
 '''
 
 import os
