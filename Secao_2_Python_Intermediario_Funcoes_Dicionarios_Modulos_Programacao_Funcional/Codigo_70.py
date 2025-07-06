d03a167017d3f2cccec4109505d1bcb72f335b04
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-06 00:01:50 -0300

    Updata Codigo_70.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_70.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_70.py
@@ -32,4 +32,14 @@ def escopo_2():
     
     def outra_funcao():
         
+        y = 2
         
+        print(x, y)
+        
+    outra_funcao()
+    
+    print(x)
+    
+print(x)
+escopo_2()
+print(x)
