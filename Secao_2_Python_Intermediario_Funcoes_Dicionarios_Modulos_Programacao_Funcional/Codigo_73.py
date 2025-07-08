34524b6cede6cd1956a53506892e848489e41860
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-07 23:24:06 -0300

    Update Codigo_73.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_73.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_73.py
@@ -6,3 +6,20 @@
 import os
 
 os.system('color 1f')
+
+def soma(*args):
+    
+    total = 0
+    
+    for numero in args:
+        
+        total += numero
+        
+    return total
+
+print('\n')
+soma(1, 2, 3, 4, 5)
+print('\n')
+
+os.system('pause')
+os.system('cls')
