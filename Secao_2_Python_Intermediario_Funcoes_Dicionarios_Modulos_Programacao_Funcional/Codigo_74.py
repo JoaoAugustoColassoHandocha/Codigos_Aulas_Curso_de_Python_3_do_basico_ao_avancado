f895c387c6c3b3b502054ad6290eb55142b50af6
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-09 23:59:06 -0300

    Update Codigo_74.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_74.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_74.py
@@ -18,25 +18,12 @@ os.system('color 1f')
 def multi(*args):
     
     total = 1
-    par_ou_impar = ''
     
     for numero in args:
         
         total *= numero
-        
-        def par_impar(total, par_ou_impar):
-            
-            if total % 2 == 0:
-                
-                par_ou_impar = 'Par'
-            
-            elif total % 2 != 0:
-                
-                par_ou_impar = 'Impar'
-                
-        par_impar()
-        
-    return total, par_ou_impar
+    
+    return total
 
 multi_dados = multi(1, 2, 3, 4, 5)
 
