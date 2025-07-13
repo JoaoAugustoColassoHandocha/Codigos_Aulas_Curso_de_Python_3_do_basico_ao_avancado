edec2707396c7a570f39fab23dc786ca8cb00fd0
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-12 23:23:26 -0300

    Update Codigo_77.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_77.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_77.py
@@ -7,7 +7,15 @@ import os
 
 os.system('color 1f')
 
-def criar_saudacao():
+def criar_saudacao(saudacao):
+
+    def saudar(nome):
+        
+        return f'{saudacao}, {nome}!'
+    
+    return saudar
+
+
 
 os.system('pause')
 os.system('cls')
