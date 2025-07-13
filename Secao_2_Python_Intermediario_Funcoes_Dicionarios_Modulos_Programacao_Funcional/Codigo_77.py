079275c612f8c8530a4cd6747197a786fb6c821f
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-12 23:28:43 -0300

    Update Codigo_77.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_77.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_77.py
@@ -19,5 +19,13 @@ falar_bom_dia = criar_saudacao('Bom dia')
 falar_boa_tarde = criar_saudacao('Boa tarde')
 falar_boa_noite = criar_saudacao('Boa noite')
 
+for nome in ['Maria', 'João', 'Davi']:
+    
+    print(f'\n{falar_bom_dia(nome)}')
+    print('---')
+    print(f'{falar_boa_tarde(nome)}')
+    print('---')
+    print(f'{falar_boa_noite(nome)}\n')
+
 os.system('pause')
 os.system('cls')
