c620af5b0613ea717a10466e2e1091460f1835c2
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-13 22:33:07 -0300

    Update Codigo_78.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_78.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_78.py
@@ -9,6 +9,14 @@ import os
 
 os.system('color 1f')
 
+def multiplica_numero(multiplicador):
+    
+    def numero_repassado(numero):
+        
+        return multiplicador * numero
+    
+    return numero_repassado
+
 
 
 os.system('pause')
