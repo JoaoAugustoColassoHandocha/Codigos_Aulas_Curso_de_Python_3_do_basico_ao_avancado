0e6bcf451e882ec3033e9aa466a304f883b09065
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-14 00:47:47 -0300

    Update Codigo_80.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_80.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_80.py
@@ -35,5 +35,20 @@ import os
 
 os.system('color 1f')
 
+pessoa = {
+
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.8,
+    'endereços': [
+        
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    
+    ],
+
+}
+
 os.system('pause')
 os.system('cls')
