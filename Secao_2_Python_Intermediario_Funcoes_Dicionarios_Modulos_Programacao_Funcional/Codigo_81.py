8e7d77be1f3013adceafc6c143c089832973f583
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-15 00:42:28 -0300

    Update Codigo_81.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_81.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_81.py
@@ -7,5 +7,20 @@ import os
 
 os.system('color 1f')
 
+pessoa = {
+    
+    'nome': 'João Augusto',
+    'sobrenome':'Colasso Handocha',
+    'idade': 27,
+    'altura': 1.80,
+    'endereços': [
+        
+        {'rua': 'tal tal', 'número': 123}
+        {'rua': 'outra rua', 'número': 345},
+        
+    ],
+        
+}
+
 os.system('pause')
 os.system('cls')
