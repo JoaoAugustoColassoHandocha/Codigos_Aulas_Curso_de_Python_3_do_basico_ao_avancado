4de437795d3e5b8ceac540dccd31d4669e48867a
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-18 03:25:24 -0300

    Update Codigo_82.py

--- Codigo_82.py
+++ Codigo_82.py
@@ -1,4 +1,15 @@
 '''
+Métodos úteis dos dicionários em Python
 
+len - quantas chaves
+keys - iterável com as chaves
+values - iteráveç com os valores
+items - iterável com chaves e valores
+setdefault - adiciona valor se a chave não existe
+copy - retorna uma cópia rasa
+get - obtém uma chave
+pop - apaga um item com a chave especificada (del)
+popitem - apaga o último item adicionado
+update - atualiza um dicionário com outro
 
 '''
