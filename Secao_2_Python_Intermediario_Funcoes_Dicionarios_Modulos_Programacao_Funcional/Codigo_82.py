5132efe0d34a0ced49c6cab70ff74bede2000902
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-18 04:54:12 -0300

    Update Codigo_82.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_82.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_82.py
@@ -6,7 +6,7 @@ keys - iterável com as chaves
 values - iteráveç com os valores
 items - iterável com chaves e valores
 setdefault - adiciona valor se a chave não existe
-copy - retorna uma cópia rasa
+copy - retorna uma cópia rasa (shallow copy)
 get - obtém uma chave
 pop - apaga um item com a chave especificada (del)
 popitem - apaga o último item adicionado
