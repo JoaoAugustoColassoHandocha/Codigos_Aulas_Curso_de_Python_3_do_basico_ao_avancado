0ed361530082f7c593e9bd0776becd25f8caa06a
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-18 05:09:47 -0300

    Update Codigo_82.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_82.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_82.py
@@ -73,7 +73,8 @@ print(pessoa['peso'])
 d1 = {
     
     'c1': 1,
-    'c2': 2,    
+    'c2': 2,
+    'l1': [0, 1, 2],    
     
 }
 
@@ -84,6 +85,7 @@ print(d1)
 
 d2 = d1.copy()
 d2['c1'] = 100
+d2['l1'][1] = 999
 print(d1)
 print(d2)
 
