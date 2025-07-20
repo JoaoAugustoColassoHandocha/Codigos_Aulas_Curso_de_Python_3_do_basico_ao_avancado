2b96aefae2c0657505132a024fcd46e242b4660d
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-19 23:41:52 -0300

    Update Codigo_83

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_83.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_83.py
@@ -9,5 +9,17 @@ import os
 
 os.system('color 1f')
 
+perguntas = [
+    
+    {
+        
+        'pergunta': 'Quanto é 2+2?',
+        'opcoes': ['1', '2', '3', '4'],
+        'resposta': '4',
+        
+    }
+    
+]
+
 os.system('pause')
 os.system('cls')
