62d42153307a4e7d4aab24ce2177baed67799602
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-20 02:06:47 -0300

    Update Codigo_84.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_84.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_84.py
@@ -8,3 +8,31 @@ Sistema de perguntas e respostas
 import os
 
 os.system('color 1f')
+
+perguntas = [
+        
+    {
+            
+        'pergunta': 'Quanto é 2 + 2?',
+        'opcoes': ['1', '2', '3', '4'],
+        'resposta': '4',
+            
+    },
+        
+    {
+            
+        'pergunta': 'Quanto é 5 X 5?',
+        'opcoes': ['25', '55', '10', '51'],
+        'resposta': '25',
+            
+    },
+        
+    {
+            
+        'pergunta': 'Quanto é 10 / 2?',
+        'opcoes': ['4', '5', '2', '1'],
+        'resposta':'5',
+            
+    },
+        
+]
