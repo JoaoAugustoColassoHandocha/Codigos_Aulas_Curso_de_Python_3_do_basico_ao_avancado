14a7f95e9ab44af31f87b4c202ea31af58b1251e
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-22 18:29:36 -0300

    Update Codigo_87.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_87.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_87.py
@@ -36,7 +36,8 @@ os.system('color 1f')
 
 s1 = set()
 s1.add('João') # Adiciona um valor no set (Não aceita mais de um valor para adicionar)
-s1.update(1) # Muito parecido com o add, mas deforma valores interados
+s1.update('Olá mundo') # Muito parecido com o add, mas deforma valores interados
+s1.update(('Augusto', 1, 2, 3)) # Para não deformar e inserir mais informações, pode ser utilizado uma tupla
 
 os.system('pause')
 os.system('cls')
