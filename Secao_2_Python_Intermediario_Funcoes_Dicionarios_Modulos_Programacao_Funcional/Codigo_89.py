0a795005bff2af872d9e5848a23d1fe134b3d634
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-23 00:20:26 -0300

    Update Codigo_89.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_89.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_89.py
@@ -7,7 +7,19 @@ import os
 
 os.system('color 1f')
 
+letras = set()
 
+while True:
+    
+    letra = input('Digite: ')
+    letras.add(letra.lower())
+    print(f'\n{letras}\n')
+    
+    if 'l' in letras:
+        
+        print('Parabéns!\n')
+        
+        break
 
 os.system('pause')
 os.system('cls')
