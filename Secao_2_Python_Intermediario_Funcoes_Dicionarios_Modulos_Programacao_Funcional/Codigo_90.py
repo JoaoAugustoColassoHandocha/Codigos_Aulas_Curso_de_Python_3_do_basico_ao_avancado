6601978e6e2597ce6dca16ed2ddb1f62e77ebf92
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-25 13:03:52 -0300

    Update Codigo_90.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_90.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_90.py
@@ -23,50 +23,40 @@ import os
 
 os.system('color 1f')
 
+def primeiro_duplicado(lista):
+    
+    ja_visto = set()
+    
+    for num in lista:
+        
+        if num in ja_visto:
+            
+            return num
+        
+        ja_visto.add(num)
+        
+    return -1
+
 lista_de_listas_de_inteiros = [
-    
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], # S1
-    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8], # S2
-    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7], # S3
-    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9], # S4
-    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7], # S5
-    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9], # S6
-    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1], # S7
-    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3], # S8
-    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7], # S9
-    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1], # S10
-    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7], # S11
-    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1], #S12
-    
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 ]
 
-def prim_dupla(listas):
+for idx, lista in enumerate(lista_de_listas_de_inteiros):
     
-    if listas == list(set(listas)):
-        
-        print(f'{listas} = -1')
+    resultado = primeiro_duplicado(lista)
     
-    elemento_unico = []
+    print(f'Lista {idx + 1}: Primeiro duplicado é {resultado}')
     
-    for elemento in listas:
-        
-        verificar = elemento in elemento_unico
-        
-        if verificar:
-            
-            print(f'{listas} = {elemento}')
-            
-        else:
-            
-            elemento_unico.append(elemento)
-
-print('\n')
-
-for listas in lista_de_listas_de_inteiros:
-    
-    prim_dupla(listas)
-
-print('\n')
-
 os.system('pause')
 os.system('cls')
