2ca6e5853b2b5a8e0a72c326503cad8c5b7a1589
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-28 18:11:42 -0300

    Update Codigo_92.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_92.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_92.py
@@ -1,5 +1,11 @@
 '''
+Funçao lmbda em Pyhton
 
+A funçãol lambda é uma função como qualquer outra em Pyhton.
+
+Porém, são funções anônimas que contém apenas uma linha.
+
+Ou seja, tudo deve ser contido dentro de uma única expressão.
 
 '''
 
