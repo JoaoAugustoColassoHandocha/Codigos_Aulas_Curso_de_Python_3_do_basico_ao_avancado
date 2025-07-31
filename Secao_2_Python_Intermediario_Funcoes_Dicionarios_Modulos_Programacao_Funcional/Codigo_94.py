7477796576271d95b4f2ccdcbbe5f236aa5f6cc1
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-07-31 18:55:12 -0300

    Update Codigo_94.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_94.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_94.py
@@ -7,7 +7,17 @@ import os
 
 os.system('color 1f')
 
+a, b = 1, 2
+a, b = b, a
 
+print(a, b)
+
+pessoa = {
+    
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+    
+}
 
 os.system('pause')
 os.system('exit')
