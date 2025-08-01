681648288a3695d5248599411b843743db42d908
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-01 18:16:07 -0300

    Update Codigo_95.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_95.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_95.py
@@ -3,6 +3,22 @@ List comprehension em Python
 
 List comprehension é uma forma rápida para criar listas a partir de iteráveis.
 
+------------------
+
+print(list(range(10))) -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+------------------
+
+lista - []
+
+for numero in range(10):
+
+    lista.append(numero)
+    
+print(lista) -> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+------------------
+
 '''
 
 import os
