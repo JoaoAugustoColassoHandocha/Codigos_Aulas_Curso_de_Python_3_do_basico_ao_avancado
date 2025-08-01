7b8482df3217cd3c0bb8b1901bf2352c2e4ed4c5
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-01 18:33:18 -0300

    Update Codigo_96.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_96.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_96.py
@@ -18,7 +18,7 @@ produtos = [
 print('\n##############################\n')
 
 novos_produtos = [produto for produto in produtos]
-print(novos_produtos)
+print(novos_produtos) # [{'nome': 'p1', 'preco': 20}, {'nome': 'p2', 'preco': 10}, {'nome': 'p3', 'preco': 30}]
 
 print('\n##############################\n')
 
