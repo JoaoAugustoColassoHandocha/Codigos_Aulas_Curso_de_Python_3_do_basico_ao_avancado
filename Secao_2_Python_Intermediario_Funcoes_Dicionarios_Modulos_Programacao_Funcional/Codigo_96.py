fdfb2ab39d5373f83032d242714dfca7a60e10b2
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-01 18:35:54 -0300

    Update Codigo_96.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_96.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_96.py
@@ -22,7 +22,7 @@ print(novos_produtos) # [{'nome': 'p1', 'preco': 20}, {'nome': 'p2', 'preco': 10
 
 print('\n##############################\n')
 
-print(*novos_produtos, sep = ' | ')
+print(*novos_produtos, sep = ' | ') # {'nome': 'p1', 'preco': 20} | {'nome': 'p2', 'preco': 10} | {'nome': 'p3', 'preco': 30}
 
 print('\n##############################\n')
 
