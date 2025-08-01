cc50bee23e8ee6e0aad1f183e4cc2c79d697847c
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-01 18:28:40 -0300

    Update Codigo_96.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_96.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_96.py
@@ -7,7 +7,13 @@ import os
 
 os.system('color 1f')
 
-
+produtos = [
+    
+    {'nome': 'p1', 'preco': 20,},
+    {'nome': 'p2', 'preco': 10,},
+    {'nome': 'p3', 'preco': 30,},
+    
+]
 
 os.system('pause')
 os.system('cls')
