4fde6a8e6348245be560f90d59d9bfd4d35794d9
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-05 18:10:19 -0300

    Update Codigo_97.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_97.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_97.py
@@ -7,9 +7,12 @@ import os
 
 os.system('color 1f')
 
+print('\n##############################\n')
+
 lista = [numeros for numeros in range(10)]
 print(lista)
 
+print('\n##############################\n')
 
 os.system('pause')
 os.system('cls')
