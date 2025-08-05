63d94add66c5133b4706212065ebcd962ca3badc
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-05 18:46:52 -0300

    Update Codigo_98.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_98.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_98.py
@@ -9,5 +9,23 @@ import os
 
 os.system('color 1f')
 
+print('\n##############################\n')
+
+# FOR sem o list comprehension
+
+lista = []
+
+for x in range(3):
+    
+    for y in range(3):
+        
+        lista.append((x, y))
+        
+print(lista)
+
+
+print('\n##############################\n')
+
+
 os.system('pause')
 os.system('cls')
