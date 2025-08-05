9ee31d534803dbea7f0f418bb125b3e55fa4ac54
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-05 18:18:11 -0300

    Update Codigo_97.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_97.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_97.py
@@ -34,6 +34,7 @@ novos_produtos_4 = [
     {**produto, 'preco': produto['preco'] * 1.05} 
     if produto['preco'] > 20 else {**produto}
     for produto in produtos
+    if produto['preco'] > 10
 
 ]
 
