4adc43759ec4f19139277b6593dcf2639cd6ccd9
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-05 18:15:56 -0300

    Update Codigo_97.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_97.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_97.py
@@ -11,12 +11,12 @@ os.system('color 1f')
 
 print('\n##############################\n')
 
-lista = [numeros for numeros in range(10)]
+lista = [numeros for numeros in range(10)] # Sem o filtro ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(lista)
 
 print('\n##############################\n')
 
-lista = [numeros for numeros in range(10) if numeros < 5]
+lista = [numeros for numeros in range(10) if numeros < 5] # Com o filtro ([0, 1, 2, 3, 4])
 print(lista)
 
 print('\n##############################\n')
