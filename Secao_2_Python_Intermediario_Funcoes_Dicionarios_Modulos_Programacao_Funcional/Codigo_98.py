8c174cc95a5be3d65c7c1def1b980dddb35bdf11
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-05 18:44:52 -0300

    Update Codigo_98.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_98.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_98.py
@@ -1,13 +1,13 @@
 '''
+List comprehension com mais de um for
 
+* O lado esquerdo do for é utilizado para o mapeamento
 
 '''
 
 import os
 
 os.system('color 1f')
-
-
 
 os.system('pause')
 os.system('cls')
