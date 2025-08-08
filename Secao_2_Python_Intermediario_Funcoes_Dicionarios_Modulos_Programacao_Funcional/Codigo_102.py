a36644438b4a2427959b22cdf92881179180bf85
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-08 18:47:52 -0300

    Update Codigo_102.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_102.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_102.py
@@ -1,8 +1,8 @@
 '''
-DIR, HASTTR, e GETATTR em Pyhton
+DIR, HASATTR, e GETATTR em Pyhton
 
 * dir: É usada para listar os atributos (como métodos, variáveis, etc.) de um objeto, retornando uma lista de nomes disponíveis em um determinado escopo ou em um objeto específico (Por exemplo, dentro da string).
-* hasttr:
+* hasattr:
 * getattr:
 
 '''
