b0160a92ff41691a68be7fe58fc30d15ae20946c
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-08 18:34:57 -0300

    Update Codigo_102.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_102.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_102.py
@@ -9,5 +9,9 @@ os.system('color 1f')
 
 string = 'João'
 
+print('\n##############################\n')
+print(string)
+print('\n##############################\n')
+
 os.system('pause')
 os.system('cls')
