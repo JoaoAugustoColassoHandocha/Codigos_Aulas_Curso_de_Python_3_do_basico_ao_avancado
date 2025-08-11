03e0a0902d06d96786c44fd94dee2c8e82509050
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-11 18:58:39 -0300

    Update Codigo_104.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_104.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_104.py
@@ -8,6 +8,8 @@ Under: __xxx__
 
 Generator expression: São funções que sabem pausar em determinada ocasião.
 
+A diferença da lista para o generator expression é que a lista está toda na memória e o generator está esperando pedir um valor.
+
 '''
 
 import os, sys
@@ -26,6 +28,22 @@ generator = (numero for numero in range(100)) # Generator expression
 print(f'{sys.getsizeof(lista)} bytes') # 920 bytes
 print('\n')
 print(f'{sys.getsizeof(generator)} bytes') # 192 bytes
+
+print('\n##############################\n')
+
+print(next(generator)) # 0
+print(next(generator)) # 1
+print(next(generator)) # 2
+
+print('\n##############################\n')
+
+for numero in generator:
+    
+    print(numero)
+
+print('\n##############################\n')
+
+print([numero for numero in generator])
 
 print('\n##############################\n')
 
