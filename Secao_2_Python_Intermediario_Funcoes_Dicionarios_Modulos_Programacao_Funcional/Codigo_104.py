631bcb42350d0fd5dec10639fb4f441cc5e5df5d
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-11 18:40:28 -0300

    Update Codigo_104.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_104.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_104.py
@@ -1,5 +1,10 @@
 '''
+Generator expression, Iterables e Iterators em Python
 
+Iterable: Tem a responsabilidade de ter os valores sequencialmente.
+Iterator: Tem a responsabilidade de entregar um valor por vez.
+
+Under: __xxx__
 
 '''
 
