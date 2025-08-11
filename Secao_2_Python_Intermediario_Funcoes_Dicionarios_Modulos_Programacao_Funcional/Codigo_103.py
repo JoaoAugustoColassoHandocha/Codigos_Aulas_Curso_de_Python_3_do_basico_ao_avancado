98989b876b1ae4290287594d945a6ddaae65e450
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-11 18:28:44 -0300

    Update Codigo_103.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_103.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_103.py
@@ -4,7 +4,7 @@ Generator expression, Iterables e Iterators em Python
 Iterável: Tem a responsabilidade de ter os valores sequencialmente.
 Iterator: Tem a responsabilidade de entregar um valor por vez.
 
-Under: __
+Under: __xxx__
 
 '''
 
