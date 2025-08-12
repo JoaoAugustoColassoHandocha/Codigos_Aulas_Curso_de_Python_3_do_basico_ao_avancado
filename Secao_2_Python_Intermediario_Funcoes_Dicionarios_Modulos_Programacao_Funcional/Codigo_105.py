d8fc1e1c430a5874e0b1f91af64443b2c9766c62
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-12 18:48:07 -0300

    Update Codigo_105.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_105.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_105.py
@@ -16,12 +16,21 @@ print('\n##############################\n')
 def generator(numero = 0):
     
     yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
     return 'Acabou' # Levanta uma excessão de StopIteration, quando é solicitado a execução do next() não tendo mais nada para executar, e exibe a mensagem inserida no return
 
 gen = generator(numero = 0)
 print(next(gen)) # 1
-print('\n')
 print(iter(gen)) # <generator object generator at 0x00000181753B5F00>
+
+print('\n##############################\n')
+
+for numero in gen:
+    
+    print(numero)
 
 print('\n##############################\n')
 
