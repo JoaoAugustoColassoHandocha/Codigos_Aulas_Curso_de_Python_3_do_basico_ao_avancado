82ebbe6b3b91fdf105cb167c0a369e4178be2b79
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-12 18:31:58 -0300

    Update Codigo_105.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_105.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_105.py
@@ -3,13 +3,27 @@ Introdução ás Generator functions em Python
 
 generator = (numero for numero in range(100))
 
+yield: Pausa a execução
+
 '''
 
 import os
 
 os.system('color 1f')
 
+print('\n##############################\n')
 
+def generator(numero = 0):
+    
+    yield 1
+    return 'Acabou'
+
+gen = generator(numero = 0)
+print(next(gen))
+print('\n')
+print(iter(gen))
+
+print('\n##############################\n')
 
 os.system('pause')
 os.system('cls')
