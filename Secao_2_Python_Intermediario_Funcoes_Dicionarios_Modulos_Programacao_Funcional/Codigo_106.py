3e692dd5679f367a3b37524385aa97231d64c3ae
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-14 18:36:32 -0300

    Update Codigo_106.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_106.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_106.py
@@ -30,5 +30,39 @@ for numero in gen:
 
 print('\n##############################\n')
 
+def ge1():
+    
+    print('COMEÇO GEN1')    
+    yield 1
+    yield 2
+    yield 3
+    print('TERMINO GEN1')
+    
+def gen2(gen):
+    
+    print('COMEÇO GEN2')
+    yield from gen()
+    yield 4
+    yield 5
+    yield 6
+    print('TERMINO GEN2')
+    
+def gen3():
+    
+    print('COMEÇO GEN3')
+    yield 10
+    yield 20
+    yield 30
+    print('TERMINO GEN3')
+    
+    
+g1 = gen2()
+
+for numero in gen:
+    
+    print(numero) # 1 a 6
+
+print('\n##############################\n')
+
 os.system('pause')
 os.system('cls')
