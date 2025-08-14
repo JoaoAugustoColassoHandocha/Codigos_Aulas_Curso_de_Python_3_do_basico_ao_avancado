7843de40f61e95ab2fe02a9bd19c699c002515d6
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-14 18:29:54 -0300

    Update  Codigo_106.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_106.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_106.py
@@ -9,7 +9,24 @@ os.system('color 1f')
 
 print('\n##############################\n')
 
+def ge1():
+    
+    yield 1
+    yield 2
+    yield 3
+    
+def gen2():
+    
+    yield from ge1()
+    yield 4
+    yield 5
+    yield 6
+    
+gen = gen2()
 
+for numero in gen:
+    
+    print(numero)
 
 print('\n##############################\n')
 
