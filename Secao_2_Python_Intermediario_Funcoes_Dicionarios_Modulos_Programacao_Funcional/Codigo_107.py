e3b0ebb0c9f9dc59c59083f4808b3c42123c34a2
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-18 18:25:20 -0300

    Update Codigo_107.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_107.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_107.py
@@ -9,7 +9,22 @@ os.system('color 1f')
 
 print('\n##############################\n')
 
-
+try:
+    
+    a = 18
+    b = 0
+    print(b[0])
+    print('Linha 1')
+    c = a / b
+    print('Linha 2')
+    
+except ZeroDivisionError:
+    
+    print('Dividiu por zero')
+    
+except NameError:
+    
+    print()
 
 print('\n##############################\n')
 
