8454f591c5732e18fa779bc325d7fc3244e2be30
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-21 18:36:14 -0300

    Udpate Codigo_109.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_109.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_109.py
@@ -43,5 +43,17 @@ print(divide(8, 0))
 
 print('\n##############################\n')
 
+def divide(n, d):
+    
+    if d == 0:
+        
+        raise ZeroDivisionError('Você está tentando dividir por 0!')
+          
+    return n / d
+
+print(divide(8, 0))
+
+print('\n##############################\n')
+
 os.system('pause')
 os.system('cls')
