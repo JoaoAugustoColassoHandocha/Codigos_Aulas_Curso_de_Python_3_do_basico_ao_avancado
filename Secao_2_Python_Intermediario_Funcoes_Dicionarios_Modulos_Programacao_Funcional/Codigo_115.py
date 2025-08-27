48f76d88e76f1b53d8cc50bbee9304336a590804
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-27 18:58:34 -0300

    Update Codigo_115.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_115.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_115.py
@@ -5,6 +5,7 @@
 
 import os
 import Codigo_114_Package.Codigo_114_Modulo
+import Codigo_114_Package.Codigo_115_Modulo
 
 os.system('color 1f')
 
@@ -15,6 +16,10 @@ print(__name__)
 print('\n******************************\n')
 
 print(Codigo_114_Package.Codigo_114_Modulo.soma(1, 1))
+
+print('\n******************************\n')
+
+print(Codigo_114_Package.Codigo_115_Modulo.fala_oi())
 
 print('\n******************************\n')
 
