878a0e1f23f479e658e60193adee33bd45297d0d
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-27 18:19:47 -0300

    Update Codigo_114.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_114.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_114.py
@@ -7,6 +7,8 @@ import os, sys
 
 import Codigo_114_Package.Codigo_114_Modulo
 
+from Codigo_114_Package.Codigo_114_Modulo import soma
+
 os.system('color 1f')
 
 print('\n******************************\n')
@@ -20,6 +22,10 @@ print(*sys.path, sep = '\n')
 print('\n******************************\n')
 
 print(f'O resultado de 1 + 2 é {Codigo_114_Package.Codigo_114_Modulo.soma(1, 2)}')
+
+print('\n******************************\n')
+
+print(f'O resultado de 1 + 2 é {soma(1, 2)}')
 
 print('\n******************************\n')
 
