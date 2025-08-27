d2b94a6eb7ae1942c0c094cc4a646dfa81d0990a
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-27 18:52:44 -0300

    Update Codigo_114_Modulo.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_114_Package/Codigo_114_Modulo.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_114_Package/Codigo_114_Modulo.py
@@ -1,5 +1,5 @@
 import os
-import Codigo_115_Modulo
+import Codigo_114_Package.Codigo_115_Modulo
 
 os.system('color 1f')
 
@@ -7,4 +7,4 @@ def soma(x, y):
     
     return x + y
 
-Codigo_115_Modulo.fala_oi() # Apresenta erro na __main__
+Codigo_114_Package.Codigo_115_Modulo.fala_oi()
