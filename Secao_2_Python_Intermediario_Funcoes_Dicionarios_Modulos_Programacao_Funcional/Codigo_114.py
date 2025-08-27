2b1a255d48a71b0ff0c85cf9fd74754ab231e5f7
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-27 18:14:26 -0300

    Update Codigo_114.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_114.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_114.py
@@ -3,13 +3,17 @@
 
 '''
 
-import os
+import os, sys
 
 os.system('color 1f')
 
 print('\n******************************\n')
 
+print(__name__)
 
+print('\n******************************\n')
+
+print(__name__)
 
 print('\n******************************\n')
 
