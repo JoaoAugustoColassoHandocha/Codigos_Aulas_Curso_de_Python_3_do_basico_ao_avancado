12f34f85e61d50db360be6de7c181e90938e7989
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-28 18:46:49 -0300

    Update Codigo_117.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
@@ -1,8 +1,9 @@
 '''
 Exercícios
 
-# Aumente os preços dos produtos a seguir em 10%
-# Gere novos_produtos por deep copy (cópia profunda)
+* Aumente os preços dos produtos a seguir em 10%
+* Gere novos_produtos por deep copy (cópia profunda)
+
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
     {'nome': 'Produto 1', 'preco': 22.32},
@@ -11,11 +12,11 @@ produtos = [
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
-# Ordene os produtos por nome decrescente (do maior para menor)
-# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+* Ordene os produtos por nome decrescente (do maior para menor)
+* Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
-# Ordene os produtos por preco crescente (do menor para maior)
-# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+* Ordene os produtos por preco crescente (do menor para maior)
+* Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 
 '''
 
