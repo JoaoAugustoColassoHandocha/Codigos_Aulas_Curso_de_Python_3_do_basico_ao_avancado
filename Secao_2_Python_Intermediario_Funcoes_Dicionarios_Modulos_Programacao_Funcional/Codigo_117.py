f0d8d21f50bf5d3f2b07216ad4aa5463b2d3b488
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-08-28 18:49:44 -0300

    Update Codigo_117.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
@@ -5,11 +5,11 @@ Exercícios
 * Gere novos_produtos por deep copy (cópia profunda)
 
 produtos = [
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 4', 'preco': 69.90},
     {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
 * Ordene os produtos por nome decrescente (do maior para menor)
