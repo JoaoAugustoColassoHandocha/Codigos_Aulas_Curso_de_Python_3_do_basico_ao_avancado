8e6194b1c1293ee04b47e9d9c3cb845544e99ae7
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-02 18:24:57 -0300

    Update Codigo_117.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
@@ -26,7 +26,30 @@ os.system('color 1f')
 
 print('\n******************************\n')
 
+try:
+    
+    a = 18
+    b = 0
+    print(b[0])
+    print('Linha 1')
+    c = a / b
+    print('Linha 2')
+    
+except ZeroDivisionError  as error:
+    
+    print(f'{error.__class__.__name__}: {error}')
+    
+except NameError  as error:
+    
+    print(f'{error.__class__.__name__}: {error}')
 
+except (TypeError, IndexError) as error:
+    
+    print(f'{error.__class__.__name__}: {error}')
+    
+except Exception  as error:
+    
+    print(f'{error.__class__.__name__}: {error}')
 
 print('\n******************************\n')
 
