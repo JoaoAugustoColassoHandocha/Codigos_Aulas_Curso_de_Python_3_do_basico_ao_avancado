1d7573f4320a4412b1ff3fefd33e5bfaf6382f96
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-03 17:02:55 -0300

    Update Codigo_118.py

---
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_118.py
@@ -0,0 +1,17 @@
+'''
+
+
+'''
+
+import os
+
+os.system('color 1f')
+
+print('\n******************************\n')
+
+
+
+print('\n******************************\n')
+
+os.system('pause')
+os.system('cls')
