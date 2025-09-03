596d1f2da05d40003bec87005c1621ac804d7de3
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2025-09-03 01:39:06 -0300

    Update Codigo_117.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_117.py
@@ -39,6 +39,7 @@ print('\n******************************\n')
 try:
     
     novos_produtos = [{**produto, 'preco': produto['preco'] * 1.10} for produto in produtos]
+    
     print(f'{novos_produtos}')
     
 except ZeroDivisionError  as error:
