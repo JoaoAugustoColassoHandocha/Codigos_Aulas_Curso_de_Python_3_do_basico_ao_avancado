fc8c66bc59b19eece186ec17bfe3a4dae45c3e93
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-03 18:15:55 -0300

    Update produtos_modulo.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_118_Package/produtos_modulo.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_118_Package/produtos_modulo.py
@@ -7,8 +7,10 @@ import os
 
 os.system('color 1f')
 
-print('\n******************************\n')
-
-
-
-print('\n******************************\n')
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
