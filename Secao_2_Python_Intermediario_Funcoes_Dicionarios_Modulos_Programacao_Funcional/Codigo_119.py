c1742a70f11dcf74484577e811a4984d65f127eb
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-10 19:21:22 -0300

    Update Codigo_119.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_119.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_119.py
@@ -32,10 +32,9 @@ num = int(input('Digite um número: '))
 soma_com_cinco = executa(soma, 5, num)
 multiplica_por_dez = executa(multiplica, 10, num)
 
-for numero in num:
-    
-    print(f'A soma de 5 + {num} é {soma_com_cinco(numero)}')
-    print(f'A multiplicação de 10 * {num} é {multiplica_por_dez(numero)}')
+ 
+print(f'A soma de 5 + {num} é {soma_com_cinco}')
+print(f'A multiplicação de 10 * {num} é {multiplica_por_dez}')
 
 print('\n******************************\n')
 
