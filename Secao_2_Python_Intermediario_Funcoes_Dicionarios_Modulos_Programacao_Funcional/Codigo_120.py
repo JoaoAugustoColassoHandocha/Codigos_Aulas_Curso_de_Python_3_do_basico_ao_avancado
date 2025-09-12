c3241f83567e473eb74ce14937377d687b418854
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-12 18:20:38 -0300

    Update  Codigo_120.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_120.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_120.py
@@ -21,6 +21,9 @@ def executa(funcao, *args):
     
     return funcao(*args)
 
+soma_com_cinco = executa(soma, 5)
+multiplica_por_dez = executa(multiplica, 10)
+
 print('\n******************************\n')
 
 os.system('pause')
