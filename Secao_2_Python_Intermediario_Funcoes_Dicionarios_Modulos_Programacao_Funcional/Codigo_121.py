50e9ff9d046d1ea4e274869c8db1a3a04a93740d
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-16 18:34:09 -0300

    Update Codigo_121.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_121.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_121.py
@@ -43,7 +43,7 @@ def concatenar(string_inicial):
     
     valor_final = string_inicial
     
-    def interna(valor_a_concatenar):
+    def interna(valor_a_concatenar = ''):
         
         nonlocal valor_final
         
