95793b5272c72a24a000a8eceebf3aaf5c4c4a6f
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-18 18:10:09 -0300

    Update Codigo_123.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_123.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_123.py
@@ -1,5 +1,11 @@
 '''
+Funções decoradoras e decoradores
 
+Decorar = Adicionar / Remover / Restringir / Alterar
+
+Funções decoradoras são funções que decoram outras funções
+
+Decoradores são usados para fazer o Python usar as funções decoradoras em outras funções.
 
 '''
 
