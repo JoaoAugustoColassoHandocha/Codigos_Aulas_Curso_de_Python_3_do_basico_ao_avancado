c10157f6dccc763f60b6eaf45d7c8b86118771dc
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-19 18:11:18 -0300

    Update Codigo_123.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_123.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_123.py
@@ -17,7 +17,45 @@ os.system('color 1f')
 
 print('\n******************************\n')
 
+def criar_funcao(func):
+    
+    def interna(*args, **kwargs):
+        
+        print('Vou te Decorar')
+        
+        for arg in args:
+            
+            e_string(arg)
+        
+        resultado = func(*args, **kwargs)
+        
+        print(f'Seu resultado foi {resultado}')        
+        print('Ok, agora você foi decorada')
+        
+        return resultado
+    
+    return interna
 
+def inverte_string(string):
+    
+    return string[::-1]
+
+def e_string(param):
+    
+    if not isinstance(param, str):
+        
+        raise TypeError('Parâmetro deve ser uma string')
+    
+invertida_string_checando_parametro = criar_funcao(inverte_string)
+
+invertida1 = invertida_string_checando_parametro('Joao') # oaoJ
+invertida2 = invertida_string_checando_parametro(123) # Apresenta erro TypeError
+
+print(invertida1)
+
+print('\n******************************\n')
+
+print(invertida2)
 
 print('\n******************************\n')
 
