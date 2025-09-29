5adb69bd5718e0243406c9f96c145ec317e91359
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-29 18:20:15 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -15,3 +15,14 @@ print('\n******************************\n')
 
 os.system('pause')
 os.system('cls')
+
+cidade = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+estado  = ['BA', 'SP', 'MG', 'RJ']
+
+combinados = zip(cidade, estado)
+
+lista_combinados = list(combinados)
+
+for cidade_estado in lista_combinados:
+           
+    print(f'{cidade_estado[0]} - {cidade_estado[1]}')
