1479a31c33a6759db955ff72d42109777e88d1dc
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-29 18:42:10 -0300

    Update Codigo_127.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_127.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_127.py
@@ -29,8 +29,8 @@ def zipper(lista1, lista2):
     
     intervalo_maximo = min(len(lista1),len(lista2))
 
-l1 =
-l2 = 
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
 
 print('\n******************************\n')
 
