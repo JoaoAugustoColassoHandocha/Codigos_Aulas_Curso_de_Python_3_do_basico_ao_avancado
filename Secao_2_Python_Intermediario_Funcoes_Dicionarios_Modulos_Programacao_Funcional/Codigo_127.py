b4e81a80921b646378aff4cfc1777abfc73bd834
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-09-29 18:34:43 -0300

    Update Codigo_127.py

---
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_127.py
@@ -0,0 +1,20 @@
+'''
+RESOLUÇÃO
+
+Unir listas
+
+Crie uma função zipper (como o zipper de roupas)
+
+O trabalho dessa função será unir duas listas na ordem.
+
+Use todos os valores da menor lista.
+
+Ex.:
+
+['Salvador', 'Ubatuba', 'Belo Horizonte'] ['BA', 'SP', 'MG', 'RJ']
+
+Resultado
+
+[('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+'''
