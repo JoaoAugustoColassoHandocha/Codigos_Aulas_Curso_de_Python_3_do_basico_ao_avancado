6f92a28d58c6ad04b813f862049b2c185a7e914a
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-01 18:39:01 -0300

    Update Codigo_129.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_129.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_129.py
@@ -1,6 +1,8 @@
 '''
 RESOLUÇÃO
 
+Considerando duas listas de inteiros ou floats (lista A e lista B), some os valores nas listas, retornando uma nova lista com os valores somados.
+
 '''
 
 import os
