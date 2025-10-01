8c2dd8810c6f02b87f49c2a44d7b268e5fd2b614
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-01 18:05:34 -0300

    Update Codigo_128.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_128.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_128.py
@@ -3,6 +3,8 @@ EXERCICIO
 
 Considerando duas listas de inteiros ou floats (lista A e lista B), some os valores nas listas, retornando uma nova lista com os valores somados.
 
+Se uma lista for maior que a outra, a soma só vai considerar o tamanho da menor.
+
 '''
 
 import os
