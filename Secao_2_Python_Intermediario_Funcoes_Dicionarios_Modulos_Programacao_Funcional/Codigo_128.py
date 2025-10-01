8ce2baf3be1d3172dc4f7db3fef47f4523813470
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-01 18:32:00 -0300

    Update Codigo_128.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_128.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_128.py
@@ -27,9 +27,6 @@ def func_soma(lista1, lista2):
     intervalo_maximo = min(len(lista1),len(lista2))
     return [(lista1[i] + lista2[i]) for i in range(intervalo_maximo)]
 
-lista_a = [1, 2, 3, 4, 5, 6, 7]
-lista_b = [1, 2, 3, 4]
-
 for lista_soma in func_soma(lista_a, lista_b):
     
     print(f'{lista_soma}')
