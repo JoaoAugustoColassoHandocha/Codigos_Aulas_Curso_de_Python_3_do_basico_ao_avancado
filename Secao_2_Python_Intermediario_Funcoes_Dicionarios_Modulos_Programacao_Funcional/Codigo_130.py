1af19847efd4533f32d5598e8542a7be8f157299
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-02 18:26:18 -0300

    Update Codigo_130.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_130.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_130.py
@@ -30,6 +30,20 @@ print('r1', hasattr(r1, '__next__')) # R1 False
 
 print('\n******************************\n')
 
+print('COUNT')
+
+for i in c1:
+    
+    if i > 30:
+        
+        break
+    
+    print(i)
+
+print('\n******************************\n')
+
+print('COUNT')
+
 for i in c1:
     
     if i > 30:
