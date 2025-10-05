7645154fd6ae30591427abcd7f45167cec3fe016
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-05 00:50:39 -0300

    Update Codigo_132.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_132.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_132.py
@@ -7,6 +7,20 @@ import os
 
 os.system('color 1f')
 
+alunos = [
+    
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+    
+]
+
 print('\n******************************\n')
 
 
