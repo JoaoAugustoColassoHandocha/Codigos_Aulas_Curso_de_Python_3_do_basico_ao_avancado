7e71bba980d5430ebff7a74ca7c4c63d14c81b14
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-15 18:36:10 -0300

    Update Codigo_134.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_134.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_134.py
@@ -10,6 +10,16 @@ os.system('color 1f')
 def print_iter(iterator):
     
     print(*list(iterator), sep = '\n')
+    
+produtos = [
+    
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+    
+]
 
 print('\n******************************\n')
 
