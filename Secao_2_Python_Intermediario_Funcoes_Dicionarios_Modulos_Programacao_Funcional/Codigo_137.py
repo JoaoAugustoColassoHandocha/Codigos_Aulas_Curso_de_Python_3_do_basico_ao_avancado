92aac46e257ccf1cd2ba579d525c575d24fa192e
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-20 18:50:14 -0300

    Update Codigo_137.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_137.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_137.py
@@ -7,9 +7,23 @@ import os
 
 os.system('color 1f')
 
+def recursiva(inicio = 0, fim = 10):
+
+    print(inicio, fim)
+    
+    # Caso Base
+    if inicio >= fim:
+        
+        return fim
+    
+    # Caso recursivo - contar até chegar ao final
+    inicio += 1
+    
+    return recursiva(inicio, fim)
+
 print('\n******************************\n')
 
-
+recursiva()
 
 print('\n******************************\n')
 
