785b1d2312f717850d88a9d12471a5ae0836009c
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-22 18:34:58 -0300

    Update Codigo_138.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_138.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_138.py
@@ -3,7 +3,7 @@ Ambientes virtuais em Python (venv)
 
 * Um ambiente virtual carrega toda a sua instalação do Python para uma pasta no caminho escolhido.
 
-Ao ativar um ambiente virtual, a instalação do ambiente virtual será usada.
+* Ao ativar um ambiente virtual, a instalação do ambiente virtual será usada.
 
 venv é o módulo que vamos usar para criar ambientes virtuais.
 
