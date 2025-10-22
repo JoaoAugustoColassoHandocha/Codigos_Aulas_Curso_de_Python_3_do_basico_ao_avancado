255dd6e81a4f2449a937ae361c407568d295c01a
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-22 18:33:28 -0300

    Update Codigo_138.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_138.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_138.py
@@ -1,7 +1,8 @@
 '''
-# Ambientes virtuais em Python (venv)
-# Um ambiente virtual carrega toda a sua instalação
-# do Python para uma pasta no caminho escolhido.
+Ambientes virtuais em Python (venv)
+
+Um ambiente virtual carrega toda a sua instalação do Python para uma pasta no caminho escolhido.
+
 # Ao ativar um ambiente virtual, a instalação do
 # ambiente virtual será usada.
 # venv é o módulo que vamos usar para
