d66e3e42d8e8930d6e906a619a196430ec2d7261
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-10-29 18:30:17 -0300

    Update Codigo_140.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_140.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_140.py
@@ -1,8 +1,10 @@
 '''
-Ativar o ambiente virtual
+Ativar o ambiente virtual:
 
 Pasta do ambiente virtual > Scripts > activate
 
 caminho_ate_a_pasta_onde_localiza_o_venv\pasta_do_ambiente_virtual\scripts\activate
+
+Desativar o ambiente virtual: deactivate
 
 '''
