ad244246ef83f9041dfd9ebb940f3760e6f71943
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-02 00:44:40 -0300

    Update Codigo_142.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_142.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_142.py
@@ -3,6 +3,6 @@ Criando o requirements.txt
 
 Usado para listar todas as bibliotecas e suas versões exatas que um projeto Python necessita para funcionar corretamente.
 
-
+pip freeze > requirements.txt
 
 '''
