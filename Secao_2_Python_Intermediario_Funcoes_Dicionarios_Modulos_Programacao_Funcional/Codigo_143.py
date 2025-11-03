568fbbaf375e39150e2177cdf62a48a148a64ea8
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-03 18:56:46 -0300

    Update Codigo_143.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
@@ -41,10 +41,12 @@ os.system('color 1f')
 caminho = 'C:\\Users\\jac0625\\Downloads\\Codigos_Aulas_Curso_de_Python_3_do_basico_ao_avancado\\Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional\\Codigo_143\\'
 
 caminho += 'Codigo_143.txt'
-
+'''
 arquivo = open(caminho, 'w')
 
 arquivo.close()
+
+'''
 
 print('\n******************************\n')
 
