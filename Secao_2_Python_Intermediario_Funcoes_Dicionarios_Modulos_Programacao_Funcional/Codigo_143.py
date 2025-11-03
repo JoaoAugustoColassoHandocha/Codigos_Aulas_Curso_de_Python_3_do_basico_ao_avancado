96cf0f9d2ed9134d56a98a3cadf248a80ec52b21
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-03 18:27:49 -0300

    Update Codigo_143.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
@@ -3,11 +3,19 @@ Criando arquivos com Python + Context Manager with
 
 * Usamos a função open para abrir um arquivo em Python (ele pode ou não existir)
 
-* Modos: r (leitura), w (escrita), x (para criação), a (escreve ao final), b (binário), t (modo texto) e + (leitura e escrita)
+* Modos: 
 
-* Context manager - with (abre e fecha)
+> r (leitura)
+> w (escrita)
+> x (para criação)
+> a (escreve ao final)
+> b (binário)
+> t (modo texto)
+> + (leitura e escrita)
+> Context manager - with (abre e fecha)
 
-* Métodos úteis
+* Métodos úteis:
+
 - write, read (escrever e ler)
 - writelines (escrever várias linhas)
 - seek (move o cursor)
