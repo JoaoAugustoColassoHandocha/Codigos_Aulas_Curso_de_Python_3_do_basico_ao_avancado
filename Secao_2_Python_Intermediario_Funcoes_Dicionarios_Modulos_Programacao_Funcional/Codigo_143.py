ff3fcd1d2ac7268474daec0d584b87cdf5b1169c
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-03 18:37:55 -0300

    Update Codigo_143.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
@@ -34,8 +34,15 @@ Criando arquivos com Python + Context Manager with
 
 '''
 
+import os
+
+os.system('color 1f')
+
 caminho = '\\Codigos_Aulas_Curso_de_Python_3_do_basico_ao_avancado\\Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional'
 
 caminho += 'Codigo_143.txt'
 
 print(caminho)
+
+os.system('pause')
+os.system('cls')
