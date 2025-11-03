f22ac011f51ceb251f3d1f664c4cd67694625b28
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-03 18:51:18 -0300

    Update Codigo_143.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_143.py
@@ -38,11 +38,11 @@ import os
 
 os.system('color 1f')
 
-caminho = 'C:\\Users\\jac0625\\Downloads\\Codigos_Aulas_Curso_de_Python_3_do_basico_ao_avancado\\Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional\\Codigo_143\\'
+caminho = 'C:\\Users\\jac0625\\Downloads\\Codigos_Aulas_Curso_de_Python_3_do_basico_ao_avancado\\Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional\\venv\\Include\\Codigo_143'
 
 caminho += 'Codigo_143.txt'
 
-arquivo = open(caminho, '+')
+arquivo = open(caminho, 'w')
 
 arquivo.close()
 
