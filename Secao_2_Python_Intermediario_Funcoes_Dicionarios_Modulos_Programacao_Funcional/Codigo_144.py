58fa65090211cc94bfa58c63f9de769d487a80d8
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-04 18:39:29 -0300

    Update Codigo_144.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_144.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_144.py
@@ -55,6 +55,9 @@ with open(caminho, 'w+') as arquivo:
     
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
+    
+with open(caminho, 'r+') as arquivo:
+    
     print(arquivo.read())
 
 print('\n******************************\n')
