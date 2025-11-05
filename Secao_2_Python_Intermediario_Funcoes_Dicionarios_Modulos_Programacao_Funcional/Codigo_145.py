6ab53758605e55b5f7d3488f38b22d48a3414a77
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-05 18:54:14 -0300

    Update Codigo_144.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_145.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_145.py
@@ -51,6 +51,7 @@ print('\n******************************\n')
 with open(caminho, 'a+') as arquivo:
     
     arquivo.write('Atenção')
+    arquivo.write('\n')
     arquivo.write('Linha 1')
     arquivo.write('\n')
     arquivo.write('Linha 2')
