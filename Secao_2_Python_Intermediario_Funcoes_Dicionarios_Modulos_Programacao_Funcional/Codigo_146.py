99cb184ce1c09853ff1643cbf75851304241cb1a
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-06 18:39:57 -0300

    Update Codigo_146.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_146.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_146.py
@@ -36,7 +36,7 @@ Criando arquivos com Python + Context Manager with
 
 # os.remove(caminho_do_arquivo) ou os.unlink(caminho_do_arquivo) - Exclui o arquivo criado
 
-import os, sys
+import os
 
 os.system('color 1f')
 
@@ -80,4 +80,3 @@ print('\narquivo.txt apagado\n')
 
 os.system('pause')
 os.system('cls')
-sys.exit()
