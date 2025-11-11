405d068fa4cafbc5c1d4a05f0d104302b7a50630
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-11 18:47:40 -0300

    Update

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/exemplo_json.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/exemplo_json.py
@@ -7,7 +7,26 @@ import os
 
 os.system('color 1f')
 
-
+pessoas = [
+    
+    {
+        
+        'nome': 'João',
+    'sobrenome': 'Augusto',
+    'enderecos': [
+        
+        {'rua': 'R1', 'numero': 32},
+        {'rua': 'R2', 'numero': 55},
+        
+    ],
+    'altura': 1.80,
+    'numeros_preferidos': (2, 4, 6, 8, 10),
+    'dev': True,
+    'nada': None,
+        
+    }
+    
+]
 
 print('\n******************************\n')
 
