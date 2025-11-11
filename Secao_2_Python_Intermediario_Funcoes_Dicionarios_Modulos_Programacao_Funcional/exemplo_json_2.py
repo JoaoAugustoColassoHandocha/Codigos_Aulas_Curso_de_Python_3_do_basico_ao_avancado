292976f71109203aad56174894d9291c397dd704
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-11 19:03:59 -0300

    Update

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/exemplo_json_2.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/exemplo_json_2.py
@@ -8,7 +8,7 @@ import os, json
 os.system('color 1f')
 
 BASE_DIR = os.path.dirname(__file__)
-SAVE_TO = os.path.join(BASE_DIR, 'aquivo_JSON.json')
+JSON_FILE = os.path.join(BASE_DIR, 'aquivo_JSON.json')
 
 os.system('pause')
 os.system('cls')
