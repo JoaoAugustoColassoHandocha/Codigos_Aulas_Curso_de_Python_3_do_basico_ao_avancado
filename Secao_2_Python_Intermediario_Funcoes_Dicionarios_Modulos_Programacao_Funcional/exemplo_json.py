34b0ab5308d91b9c3486e4727da95cfe507dbc4b
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-11 18:56:07 -0300

    Update

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/exemplo_json.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/exemplo_json.py
@@ -31,7 +31,7 @@ pessoas = [
 BASE_DIR = os.path.dirname(__file__)
 SAVE_TO = os.path.join(BASE_DIR, 'aquivo_JSON.json')
 
-with open(SAVE_TO, 'w+') as file:
+with open(SAVE_TO, 'w+', encoding = 'utf-8') as file:
     
     json.dump(pessoas, file, indent = 2)
 
