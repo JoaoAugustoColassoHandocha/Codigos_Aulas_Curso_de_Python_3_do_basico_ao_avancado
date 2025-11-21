4efc8cd6c4d153baf74a3ae4a76ea1b23391898c
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-21 16:51:36 -0300

    Update Codigo_150.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_150.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_150.py
@@ -44,7 +44,7 @@ def desfazer(tarefas, tarefas_refazer):
     tarefa = tarefas.pop()
     tarefas_refazer.append(tarefa)
     
-    print('Tarefa Desfeita')
+    print('\nTarefa Desfeita')
     
 def refazer(tarefas, tarefas_refazer):
     
@@ -56,7 +56,7 @@ def refazer(tarefas, tarefas_refazer):
     tarefa = tarefas_refazer.pop()
     tarefas.append(tarefa)
     
-    print('Tarefa Refeita')
+    print('\nTarefa Refeita')
     
 def adicionar(tarefa, tarefas):
     
