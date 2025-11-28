f9aea1d44571bcdcd43125a6badf8cb584a2fb95
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-28 18:28:48 -0300

    Update Codigo_151.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_151.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_151.py
@@ -74,7 +74,7 @@ while True:
                 
         'listar': lambda: listar(tarefas),
         'desfazer': lambda: desfazer(tarefas, tarefas_refazer),
-        'refazer': refazer(tarefas, tarefas_refazer),
+        'refazer': lambda: refazer(tarefas, tarefas_refazer),
         'clear': os.system('cls' if os.name == 'nt' else 'clear'),
         'adicionar': adicionar(tarefa, tarefas),
         
