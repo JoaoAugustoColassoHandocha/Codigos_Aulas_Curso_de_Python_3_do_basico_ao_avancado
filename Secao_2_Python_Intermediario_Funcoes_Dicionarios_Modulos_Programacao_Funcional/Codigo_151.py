fb1ea93005ff17e002049d2a414a8e573497ca2f
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-11-28 18:21:46 -0300

    Update Codigo_151.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_151.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_151.py
@@ -5,11 +5,67 @@
 
 import os
 
-print('\n******************************\n')
+tarefas = []
+tarefas_refazer = []
 
+def listar(tarefas):
+    
+    if not tarefas:
+        
+        print('\nNenhuma tarefa para listar\n')
+        return
+    
+    print('\nTarefas:\n')
+    
+    for tarefa in tarefas:
+        
+        print(f'\t{tarefa}')
+        
+    print('\n')
+    
+def desfazer(tarefas, tarefas_refazer):
+    
+    if not tarefas:
+        
+        print('\nNenhuma tarefa para desfazer\n')
+        return
+    
+    tarefa = tarefas.pop()
+    tarefas_refazer.append(tarefa)
+    
+    print(f'\nTarefa "{tarefa}" desfeita\n')
+    
+def refazer(tarefas, tarefas_refazer):
+    
+    if not tarefas_refazer:
+        
+        print('\nNenhuma tarefa para refazer\n')
+        return
+    
+    tarefa = tarefas_refazer.pop()
+    tarefas.append(tarefa)
+    
+    print(f'\nTarefa "{tarefa}" refeita\n')
+    
+def adicionar(tarefa, tarefas):
+    
+    tarefa = tarefa.strip()
+    
+    if not tarefa:
+        
+        print('\nVocê não digitou uma tarefa.\n')
+        return
+    
+    tarefas.append(tarefa)
+    
+    print(f'\nTarefa "{tarefa}" adicionada\n')
+    input()
 
-
-print('\n******************************\n')
-
-input('Clique em qualquer tecla para continuar...')
-os.system('cls' if os.name == 'nt' else 'clear')
+while True:
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    print('\nComandos: listar desfazer e refazer')
+    tarefa = input('\nDigite uma tarefa ou comando: ')
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
