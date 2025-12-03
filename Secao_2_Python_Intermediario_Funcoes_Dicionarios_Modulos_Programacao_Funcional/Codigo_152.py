876c2c56804e09a75374b19d592beaa33c7d39df
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-03 12:48:09 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -40,7 +40,7 @@ def menu(op = 0):
         
         lista_tarefas_adicionadas.append(task)
         
-        with open('.\\Codigo_152\\Codigo_152_lista_tarefas.json', 'a+', encoding = 'utf-8') as tarefas_add:
+        with open('Codigo_152\\Codigo_152_lista_tarefas.json', 'a+', encoding = 'utf-8') as tarefas_add:
             
             json.dump(lista_tarefas_adicionadas, tarefas_add, ensure_ascii = False, indent = 2)
         
@@ -49,7 +49,7 @@ def menu(op = 0):
 
     elif op == '2':
         
-        with open('.\\Codigo_152\\Codigo_152_lista_tarefas.json', 'r+', encoding = 'utf-8') as tarefas_lista:
+        with open('Codigo_152\\Codigo_152_lista_tarefas.json', 'r+', encoding = 'utf-8') as tarefas_lista:
             
             lista_tarefas = json.load(tarefas_lista)
         
