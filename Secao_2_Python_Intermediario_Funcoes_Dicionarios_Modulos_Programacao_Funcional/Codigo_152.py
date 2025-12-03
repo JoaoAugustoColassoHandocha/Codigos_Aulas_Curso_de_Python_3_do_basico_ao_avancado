cc0dc187ca8fc6bfe349a1604013524758fdd376
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-03 00:43:15 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -163,6 +163,7 @@ def menu(op = 0):
         print('\nSaindo...\n')
         input('Clique qualquer tecla para continuar...')
         os.system('cls' if os.name == 'nt' else 'clear')
+        sys.exit()
 
     elif op != '1' or op != '2' or op != '3' or op != '4' or op != '5' or op != '6':
 
