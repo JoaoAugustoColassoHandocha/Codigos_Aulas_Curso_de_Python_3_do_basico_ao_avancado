9e9b8f73d6842c9f2325777c64564d2c26b077e0
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-06 22:53:11 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -14,6 +14,7 @@ BASE_DIR = os.path.dirname(__file__)
 SAVE_TO = os.path.join(BASE_DIR, 'Codigo_152_lista_tarefas.json')
 JSON_FILE_TASK = os.path.join(BASE_DIR, 'Codigo_152_lista_tarefas.json')
 JSON_FILE_LIXEIRA = os.path.join(BASE_DIR, 'Codigo_152_lixeira_tarefas.json')
+JSON_EXCLUIR_TAREFA = os.path.join(BASE_DIR, 'Codigo_152_lista_tarefas.json')
 
 def carregar_tarefas(caminho_arquivo):
     
