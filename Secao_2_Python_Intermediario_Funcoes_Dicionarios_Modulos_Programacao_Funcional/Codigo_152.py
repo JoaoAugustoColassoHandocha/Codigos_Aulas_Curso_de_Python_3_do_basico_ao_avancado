b88cb4ee2840aa0a8172599d49756a7f5dac4343
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-06 22:40:03 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -133,7 +133,7 @@ def menu(op = 0):
                 
                 print(lixeira)
     
-            print('\n' + '*' * 25 + '\n')
+            print('\n' + '*' * 38 + '\n')
 
             input('Clique qualquer tecla para continuar...')
             os.system('cls' if os.name == 'nt' else 'clear')
