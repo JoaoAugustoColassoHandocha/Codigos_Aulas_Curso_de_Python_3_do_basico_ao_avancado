a17ff76ad92c09a7037188e5c16d432f6ced9125
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-08 00:47:45 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -221,7 +221,7 @@ def menu(op = 0):
 
         mover_tarefa_para_lixeira(task_to_be_removed, JSON_FILE_TASK, JSON_FILE_LIXEIRA)
         
-        ...
+        ##################
         
         input('Clique qualquer tecla para continuar...')
         os.system('cls' if os.name == 'nt' else 'clear')        
