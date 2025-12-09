e10ded5eea144260768f3d83ec433e30a710a4e1
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-09 19:12:53 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -18,7 +18,7 @@ def carregar_tarefas(caminho_arquivo):
     
     try:
         
-        with open(caminho_arquivo, 'r', encoding='utf-8') as f:
+        with open(caminho_arquivo, 'r+', encoding='utf-8') as f:
             
             return json.load(f)
         
