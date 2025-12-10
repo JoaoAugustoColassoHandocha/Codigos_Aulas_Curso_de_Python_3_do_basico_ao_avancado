1fa1dcd15bc71ce8eff17becad9d4c7413f0aeeb
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-10 14:53:58 -0300

    Update Codigo_154.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_154.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_154.py
@@ -12,3 +12,4 @@ print('\n******************************\n')
 print('\n******************************\n')
 
 input('Clique em qualquer tecla para continuar...')
+os.system('cls' if os.name == 'nt' else 'clear')
