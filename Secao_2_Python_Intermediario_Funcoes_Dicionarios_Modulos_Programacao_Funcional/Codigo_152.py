9d5ead7817c6d73af49202d4db3f0abba0b8e2b4
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2025-12-11 12:11:14 -0300

    Update Codigo_152.py

--- Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
+++ Secao_2_Python_Intermediario_Funcoes_Dicionarios_Modulos_Programacao_Funcional/Codigo_152.py
@@ -4,7 +4,6 @@ Exercício - Salvando a lista de tarefas em JSON
 '''
 
 import os, json, sys
-from datetime import datetime
 
 BASE_DIR = os.path.dirname(__file__)
 JSON_ADD_TAREFAS = os.path.join(BASE_DIR, 'Codigo_152_lista_tarefas.json')
