8d0621caf5112191ff68bf34553e8c8127bbe5aa
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-01-01 20:17:15 -0300

    Update Codigo_155.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_155.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_155.py
@@ -8,7 +8,7 @@ import os
 print('\n------------------------------\n')
 
 
-print('\n******************************\n')
+print('\n------------------------------\n')
 
 input('Clique em qualquer tecla para continuar...')
 os.system('cls' if os.name == 'nt' else 'clear')
