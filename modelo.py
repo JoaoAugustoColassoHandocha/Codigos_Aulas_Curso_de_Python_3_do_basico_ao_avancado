fcce141a0cc69a15b8ef5d148336fea6772f54d8
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-01-01 20:16:21 -0300

    Update modelo.py

--- modelo.py
+++ modelo.py
@@ -9,7 +9,7 @@ print('\n------------------------------\n')
 
 
 
-print('\n******************************\n')
+print('\n------------------------------\n')
 
 input('Clique em qualquer tecla para continuar...')
 os.system('cls' if os.name == 'nt' else 'clear')
