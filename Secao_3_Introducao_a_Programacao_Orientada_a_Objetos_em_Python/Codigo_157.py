e0e137597ad14f15b716d4553228e4e5fea57de4
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-07-21 03:44:17 +0000

    Update Codigo_157.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_157.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_157.py
@@ -21,9 +21,11 @@ class Carro:
         
 fusca = Carro('Fusca')
 print(fusca.nome)
+fusca.acelerar()
 
 celta = Carro('Celta')
 print(celta.nome)
+celta.acelerar()
 
 print('\n------------------------------\n')
 
