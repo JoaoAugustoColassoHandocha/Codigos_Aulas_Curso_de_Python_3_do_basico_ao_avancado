97ec2524ba2c6e45c4cfca53428353085393fb87
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-07-27 05:44:33 +0000

    Update Codigo_159.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_159.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_159.py
@@ -7,11 +7,9 @@ import os
 
 print('\n------------------------------\n')
 
-
+class 
 
 print('\n------------------------------\n')
 
 input('Clique em qualquer tecla para continuar...')
 os.system('cls' if os.name == 'nt' else 'clear')
-
-class 
