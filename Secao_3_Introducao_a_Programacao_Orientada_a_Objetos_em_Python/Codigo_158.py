a92e57473ce34b45f6107c279cdb5182cba0cebb
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-07-27 05:33:10 +0000

    Update Codigo_158.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_158.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_158.py
@@ -8,7 +8,15 @@ import os
 
 print('\n------------------------------\n')
 
-
+class Carro:
+    
+    def __init__(self, nome):
+        
+        self.nome = nome
+        
+    def acelerar(self):
+        
+          print(f'{self.nome} está acelerando...') 
 
 print('\n------------------------------\n')
 
