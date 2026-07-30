ce3ca92c94a10b73f9bf3128336bade1d14b2689
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-07-30 04:18:33 +0000

    Update Codigo_162.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_162.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_162.py
@@ -31,6 +31,8 @@ print(f'Ano de nascimento p1: {p1.get_ano_nascimento()}\n')
 print(f'{p1.__dict__}\n')
 print(f'{vars(p1)}\n')
 
+p1.__dict__['Outra'] = 'Coisa'
+
 
 print('\n------------------------------\n')
 
