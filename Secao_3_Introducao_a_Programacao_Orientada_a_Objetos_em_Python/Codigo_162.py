551502dc24bde1f55e2b0b2d854d4da2e0f93d30
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-07-30 04:14:37 +0000

    Update Codigo_162.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_162.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_162.py
@@ -9,7 +9,25 @@ import os
 
 print('\n------------------------------\n')
 
+class Pessoa:
+    
+    ano_atual = date.today().year 
+    
+    def __init__(self, nome, idade):
+        
+        self.nome = nome
+        self.idade = idade
+        
+    def get_ano_nascimento(self):
+               
+        return Pessoa.ano_atual - self.idade
+    
+p1 = Pessoa('João', 35)
+p2 = Pessoa('Helena', 12)
 
+print(f'Ano Atual: {Pessoa.ano_atual}')
+print(f'Ano de nascimento p1: {p1.get_ano_nascimento()}')
+print(f'Ano de nascimento p2: {p2.get_ano_nascimento()}')
 
 print('\n------------------------------\n')
 
