d58cb5f573c57d9163ed7fe94c5682ba062da710
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-02 06:19:25 +0000

    Update Codigo_163.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_163.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_163.py
@@ -12,7 +12,7 @@ import os, json, sys
 BASE_DIR = os.path.dirname(__file__)
 JSON_ADD_DADOS = os.path.join(BASE_DIR, 'Codigo_163_lista_dados.json')
 JSON_FILE_TASK = os.path.join(BASE_DIR, 'Codigo_163_lista_dados.json')
-JSON_FILE_LIXEIRA = os.path.join(BASE_DIR, 'Codigo_163_lixeira_tarefas.json')
+JSON_FILE_LIXEIRA = os.path.join(BASE_DIR, 'Codigo_163_lixeira_dados.json')
 
 
 def carregar_tarefas(caminho_arquivo):
