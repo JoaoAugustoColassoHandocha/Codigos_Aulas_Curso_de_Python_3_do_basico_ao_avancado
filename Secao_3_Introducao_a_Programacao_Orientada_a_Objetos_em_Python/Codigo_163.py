7e73f558672bbc5834bd4cc8d57696a20406f1d8
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-08 05:33:16 +0000

    Update Codigo_163.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_163.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_163.py
@@ -186,7 +186,7 @@ def menu(op = 0):
             
         os.system('cls' if os.name == 'nt' else 'clear')
         
-        for info_dados in dados:
+        for info_dados in json.loads(dados.__dict__):
             
             print(f'\nOs dados "ID: {info_dados['id']} - Nome: {info_dados['nome']} - Idade: {info_dados['idade']} - Gênero: {info_dados['genero']}" adicionados com sucesso!\n')
         
