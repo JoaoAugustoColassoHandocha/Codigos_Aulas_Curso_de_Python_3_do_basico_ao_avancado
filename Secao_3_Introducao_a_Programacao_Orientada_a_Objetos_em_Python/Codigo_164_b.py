3d4e2146a9ac69d3edb4983bb3b0b4909a69ec05
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-24 20:51:31 +0000

    Update Codigo_164_b.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_164_b.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_164_b.py
@@ -10,7 +10,11 @@ print('\n------------------------------\n')
 
 with open(CAMINHO_ARQUIVO, 'r') as arquivo:
     
+    pessoas = json.load(arquivo)
     
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+    p3 = Pessoa(**pessoas[2])
 
 print('\n------------------------------\n')
 
