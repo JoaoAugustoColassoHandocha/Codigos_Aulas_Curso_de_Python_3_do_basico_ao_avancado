02a40eed8bd76c9368eb49b8e3fa355d0f2b82bd
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-28 19:50:35 +0000

    Update Codigo_167.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_167.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_167.py
@@ -1,5 +1,8 @@
 '''
-
+# method vs @classmethod vs @staticmethod
+# method - self, método de instância
+# @classmethod - cls, método de classe
+# @staticmethod - método estático (❌self, ❌cls)
 
 '''
 
