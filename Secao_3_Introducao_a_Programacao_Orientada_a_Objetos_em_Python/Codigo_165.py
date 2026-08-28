4ba057a71401dd575e2bedfb602c1192e50fdf4c
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-28 19:14:14 +0000

    Update Codigo_165.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_165.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_165.py
@@ -1,5 +1,5 @@
 '''
-Métodos de classe
+Métodos de classe + factories (fábricas)
 
 São métodos onde "self" será "cls", ou seja, ao invés de receber a instância no primeiro parâmetro, recebemosa própria classe.
 
