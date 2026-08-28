0b0e97f84700e8b8ad22f87e4b49402bb56988b0
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-28 19:46:01 +0000

    Update Codigo_166.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_166.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_166.py
@@ -21,6 +21,12 @@ class Classe:
 def funcao(*args, **kwargs):
         
         print('Oi', args, kwargs)
+        
+c1 = Classe()
+c1.funcao_que_esta_na_classe(1, 2, 3)
+funcao(1, 2, 3)
+Classe.funcao_que_esta_na_classe(nomeado = 1)
+funcao(nomeado = 1)
 
 print('\n------------------------------\n')
 
