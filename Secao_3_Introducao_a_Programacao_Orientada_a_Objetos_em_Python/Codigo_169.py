8059b88aa5fd51fdaac47a939d929bfcc73a84dd
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-31 18:38:03 +0000

    Update Codigo_169.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_169.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_169.py
@@ -14,7 +14,19 @@ import os
 
 print('\n------------------------------\n')
 
-
+class Caneta:
+    
+    def __init__(self, cor):
+        
+        self.cor_tinta = cor
+        
+    @property
+    def cor(self):
+        
+        print('PROPERTY')
+        return self.cor_tinta
+    
+caneta = Caneta('Azul')
 
 print('\n------------------------------\n')
 
