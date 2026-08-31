ed6ab88283fd32e9465910dcf77472a869feabba
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-31 20:18:02 +0000

    Update Codigo_170.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_170.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_170.py
@@ -50,8 +50,9 @@ print(f._metodo_protected())
 
 print('\n')
 
-print(f.__private)
+print(f._Foo__private)
 print(_Foo__private())
+print(f.__private) # AttributeError: 'Foo' object has no attribute '__private'. Did you mean: '_Foo__private'?
 print(__metodo_private()) # AttributeError: 'Foo' object has no attribute '__private'. Did you mean: '_Foo__private'?
        
 print('\n------------------------------\n')
