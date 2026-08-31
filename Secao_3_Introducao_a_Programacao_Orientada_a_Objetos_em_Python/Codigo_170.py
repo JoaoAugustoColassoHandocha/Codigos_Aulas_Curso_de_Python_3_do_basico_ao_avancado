39476b9103db7fe4e41f4fee195a2f54c808bdfe
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-31 20:08:19 +0000

    Update Codigo_170.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_170.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_170.py
@@ -7,7 +7,7 @@ Python NÃO TEM modificadores de acesso, mas podemos seguir as seguintes conven�
 
 _ (um underline) = protected - não DEVE ser usado fora da classe ou suas subclasses.
 
-__ (dois underlines) = private - "name mangling" (desfiguração de nomes) em Python _NomeClasse__nome_attr_ou_method só DEVE ser usado na classe em que foi declarado.
+__ (dois underlines) = private - "name mangling" (desfiguração de nomes) em Python _NomeClasse__nome_attr_ou_method só DEVE ser usado na classe em que foi declarado, onde caso queria utilizar o atributo a uma classe filha, não poderá usar os dois underlines.
 
 Convensão - é um conjunto de regras e boas práticas aceitas pela comunidade para escrever códigos limpos, legíveis e padronizados.
 
@@ -23,6 +23,7 @@ class Foo:
         
         self.public = 'Isso é público'
         self._protected = 'Isso é protegido' # Convensão
+        self.
         
     def metodo_publico(self):
         
