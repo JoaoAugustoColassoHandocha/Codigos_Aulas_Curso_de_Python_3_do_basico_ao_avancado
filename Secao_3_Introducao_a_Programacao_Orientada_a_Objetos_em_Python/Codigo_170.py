eb9c5add68bd28703378b0bd59137c98915225a9
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-31 19:53:31 +0000

    Update Codigo_170.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_170.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_170.py
@@ -22,7 +22,7 @@ class Foo:
     def __init__(self):
         
         self.public = 'Isso é público'
-        self._protected = 'Isso é protegido'
+        self._protected = 'Isso é protegido' # 
         
     def metodo_publico(self):
         
