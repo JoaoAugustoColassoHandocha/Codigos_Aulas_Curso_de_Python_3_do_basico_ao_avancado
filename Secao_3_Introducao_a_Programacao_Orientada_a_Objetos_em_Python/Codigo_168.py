2ac0ce9c5a5fb93ac41dceffda5fd0accc445456
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-31 16:06:02 +0000

    Update Codigo_168.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_168.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_168.py
@@ -1,5 +1,22 @@
 '''
+# @property - um getter no modo Pythônico
 
+# getter - um método para obter um atributo
+
+# cor -> get_cor()
+
+# modo pythônico - modo do Python de fazer coisas
+
+@property é uma propriedade do objeto, ela é um método que se comporta como um atributo 🤯 🤯 🤯
+
+Geralmente é usada nas seguintes situações:
+
+- como getter
+- p/ evitar quebrar código cliente
+- p/ habilitar setter
+- p/ executar ações ao obter um atributo
+
+Código cliente - é o código que usa seu código
 
 '''
 
