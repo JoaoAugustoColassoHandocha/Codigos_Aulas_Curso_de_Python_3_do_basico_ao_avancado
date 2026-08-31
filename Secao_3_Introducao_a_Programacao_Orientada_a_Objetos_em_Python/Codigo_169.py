e2ce2f4e328d876ec809beaab8a5410c0cdce269
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-08-31 17:02:18 +0000

    Update Codigo_169.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_169.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_169.py
@@ -1,5 +1,12 @@
 '''
-
+# @property + @setter - getter e setter no modo Pythônico
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Atributos que começar com um ou dois underlines
+# não devem ser usados fora da classe.
+#  🐍🤓🤯🤯🤯🤯
 
 '''
 
