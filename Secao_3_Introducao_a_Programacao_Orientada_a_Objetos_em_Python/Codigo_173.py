34528d45ff6596316995b01f88a270960cebfd08
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-02 20:02:20 +0000

    Update Codigo_173.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_173.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_173.py
@@ -18,7 +18,7 @@ class Cliente:
         
     def inserir_endereco(self, rua, numero):
         
-        self.enderecos.append()
+        self.enderecos.append(Endereco(rua, numero))
         
 class Endereco:
     
