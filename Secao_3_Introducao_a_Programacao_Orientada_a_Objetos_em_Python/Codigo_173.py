ac2db0071352ae34f5a4b77a22c1919eca3ced5d
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-02 19:24:26 +0000

    Update Codigo_173.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_173.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_173.py
@@ -1,9 +1,9 @@
 '''
-# Relações entre classes: associação, agregação e composição
-# Composição é uma especialização da agregação.
-# Mas nela, quando o objeto "pai" for apagado, todas
-# as referências dos objetos filhos também são
-# apagadas.
+Relações entre classes: associação, agregação e composição
+
+Composição é uma especialização da agregação.
+
+Mas nela, quando o objeto "pai" for apagado, todas as referências dos objetos filhos também são apagadas.
 
 '''
 
