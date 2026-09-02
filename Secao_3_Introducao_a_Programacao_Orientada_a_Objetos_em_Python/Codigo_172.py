3e247286fefc4fb326b762bd7e2eec0573b58811
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-02 19:14:37 +0000

    Update Codigo_172.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_172.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_172.py
@@ -24,10 +24,8 @@ class Carrinho:
         return sum([p.preco for p in self._produtos])
     
     def inserir_produtos(self, *produtos):
-        
-        for produto in produtos:
             
-            self._produtos.append(produto)
+            self._produtos.extend(produtos)
     
     def listar_produtos(self):
         
@@ -41,10 +39,14 @@ class Produto:
         
         self.nome = nome
         self.preco = preco
+        
+carrinho = Carrinho()
+p1, p2 = Produto('Caneta', 1.20), Produto('Camiseta', 20.00)
+carrinho.inserir_produtos(p1, p2)
 
 print('\n------------------------------\n')
 
-
+carrinho.listar_produtos()
 
 print('\n------------------------------\n')
 
