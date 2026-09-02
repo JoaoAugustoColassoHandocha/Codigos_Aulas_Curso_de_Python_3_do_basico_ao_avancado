8883e3f0f31336f4999ccdc3e62b6aeddcd314fc
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-02 16:48:50 +0000

    Update Codigo_171.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_171.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_171.py
@@ -26,6 +26,11 @@ class Escritor:
     def ferramenta(self):
         
         return self._ferramenta
+    
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        
+        self._ferramenta = ferramenta
         
 class FerramentaDeEscrever:
     
