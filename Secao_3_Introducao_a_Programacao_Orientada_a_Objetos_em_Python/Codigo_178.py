a3f3a98fb87eff22d6b5dd69e00acb1d80dbf16c
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-10 18:03:04 +0000

    Update Codigo_178.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_178.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_178.py
@@ -1,5 +1,9 @@
 '''
-
+# super() e a sobreposição de membros - Python Orientado a Objetos
+# Classe principal (Pessoa)
+#   -> super class, base class, parent class
+# Classes filhas (Cliente)
+#   -> sub class, child class, derived class
 
 '''
 
