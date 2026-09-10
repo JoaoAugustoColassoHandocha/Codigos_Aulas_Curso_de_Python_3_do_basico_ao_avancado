08f674b66a4bef18b7324652e8b37c02bf8b3762
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-10 17:46:16 +0000

    Update Codigo_177.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_177.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_177.py
@@ -44,8 +44,8 @@ a1 = Aluno('João', 'Augusto')
     
 print('\n------------------------------\n')
 
-c1.falar_nome_classe
-a1.falar_nome_classe
+c1.falar_nome_classe()
+a1.falar_nome_classe()
 
 print('\n------------------------------\n')
 
