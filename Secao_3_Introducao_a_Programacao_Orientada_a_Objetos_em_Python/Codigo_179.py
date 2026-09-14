a3e121b3a075a18d11d854ea1e2f222e47eb0414
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-14 19:37:23 +0000

    Update Codigo_179.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_179.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_179.py
@@ -17,13 +17,13 @@ A, B, C, D
 
 D(B, C) - C(A) - B(A) - A
 
-# método -> falar
-#           A
-#         /   \
-#        B     C
-#         \   /
-#           D
-#
+método -> falar
+           A
+         /   \
+        B     C
+         \   /
+           D
+
 # Python 3 usa C3 superclass linearization
 # para gerar o mro.
 # Você não precisa estudar isso (é complexo)
