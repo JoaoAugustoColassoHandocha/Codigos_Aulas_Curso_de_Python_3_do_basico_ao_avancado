f5da80a1491138462cccef7ec4d8cea9f786353a
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-14 19:36:27 +0000

    Update Codigo_179.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_179.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_179.py
@@ -1,8 +1,7 @@
 '''
 Herança Múltipla - Python Orientado a Objetos
 
-# Quer dizer que no Python, uma classe pode estender
-# várias outras classes.
+Quer dizer que no Python, uma classe pode estender várias outras classes.
 # Herança simples:
 # Animal -> Mamifero -> Humano -> Pessoa -> Cliente
 # Herança múltipla e mixins
