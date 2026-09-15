836b4be29936d5ee4de31e0757ed7bb84b388815
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-15 20:15:21 +0000

    Update Codigo_179.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_179.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_179.py
@@ -48,6 +48,3 @@ print('\n------------------------------\n')
 
 
 print('\n------------------------------\n')
-
-input('Clique em qualquer tecla para continuar...')
-os.system('cls' if os.name == 'nt' else 'clear')
