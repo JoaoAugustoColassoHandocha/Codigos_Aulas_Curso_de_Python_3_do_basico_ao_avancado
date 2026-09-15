800de444a6e3d09ee21f493cbf6c48085306864d
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-15 20:37:32 +0000

    Update Codigo_181.py

---
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/log.py
@@ -0,0 +1,15 @@
+'''
+Update Codigo_181.py
+
+'''
+
+import os
+
+print('\n------------------------------\n')
+
+
+
+print('\n------------------------------\n')
+
+input('Clique em qualquer tecla para continuar...')
+os.system('cls' if os.name == 'nt' else 'clear')
