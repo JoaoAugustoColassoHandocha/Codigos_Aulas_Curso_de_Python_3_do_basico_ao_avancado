3ddceb0723666b4f89cc4c0ecd40c8dbb464a621
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-15 20:30:30 +0000

    Update Codigo_181.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181.py
@@ -2,3 +2,14 @@
 Update Codigo_181.py
 
 '''
+
+import os
+
+print('\n------------------------------\n')
+
+
+
+print('\n------------------------------\n')
+
+input('Clique em qualquer tecla para continuar...')
+os.system('cls' if os.name == 'nt' else 'clear')
