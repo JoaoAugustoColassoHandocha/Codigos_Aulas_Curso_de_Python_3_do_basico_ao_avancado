8459cbc4fc0174d8cea0905c73b2a6276ea86de9
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-15 20:41:29 +0000

    Update Codigo_181.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/main.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/main.py
@@ -4,12 +4,3 @@ Update Codigo_181.py
 '''
 
 import os
-
-print('\n------------------------------\n')
-
-
-
-print('\n------------------------------\n')
-
-input('Clique em qualquer tecla para continuar...')
-os.system('cls' if os.name == 'nt' else 'clear')
