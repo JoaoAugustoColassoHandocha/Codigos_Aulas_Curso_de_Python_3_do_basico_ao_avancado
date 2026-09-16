bf15957df21874b0c7de70ecb39b735858e14838
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-16 19:03:44 +0000

    Update Codigo_181.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/eletronico.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/eletronico.py
@@ -17,6 +17,12 @@ class Eletronico:
         if not self._ligado:
             
             self._ligado = True
+            
+    def desligar(self):
+            
+            if self._ligado:
+                
+                self._ligado = False
 
 print('\n------------------------------\n')
 
