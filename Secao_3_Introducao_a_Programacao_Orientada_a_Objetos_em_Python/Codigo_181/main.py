7718b7d6e1e43ccbc3fa10c072aeb0f9289399b8
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-16 18:49:04 +0000

    Update Codigo_181.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/main.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/main.py
@@ -5,7 +5,7 @@ Update Codigo_181.py
 
 from log import LogFileMixin, LogPrintMixin
 
-lf = LogFileMixin()
+'''lf = LogFileMixin()
 lp = LogPrintMixin()
     
 lf._log('Qualquer coisa')
@@ -13,4 +13,4 @@ lp._log('Qualquer coisa')
 lf.log_error('Qualquer coisa')
 lp.log_error('Qualquer coisa')
 lf.log_success('Qualquer coisa')
-lp.log_success('Qualquer coisa')
+lp.log_success('Qualquer coisa')'''
