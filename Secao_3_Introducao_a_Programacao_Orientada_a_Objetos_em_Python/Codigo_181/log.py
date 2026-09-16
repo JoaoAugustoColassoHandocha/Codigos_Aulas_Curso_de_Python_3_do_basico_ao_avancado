53ae115e8250c59054e4743284d8118f323efb4a
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-16 18:54:43 +0000

    Update Codigo_181.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/log.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_181/log.py
@@ -44,8 +44,6 @@ class LogPrintMixin(Log):
         
         print(f'{msg} - {self.__class__.__name__}')
 
-print('\n------------------------------\n')
-
 if __name__ == '__main__':
 
     lf = LogFileMixin()
@@ -57,8 +55,6 @@ if __name__ == '__main__':
     lp.log_error('Qualquer coisa')
     lf.log_success('Qualquer coisa')
     lp.log_success('Qualquer coisa')
-
-print('\n------------------------------\n')
 
 input('Clique em qualquer tecla para continuar...')
 os.system('cls' if os.name == 'nt' else 'clear')
