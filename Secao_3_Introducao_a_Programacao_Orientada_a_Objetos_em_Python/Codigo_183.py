abd2202cbb8aba8383c09666ab7bb809ce74d6d4
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-17 19:59:21 +0000

    Update Codigo_183.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_183.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_183.py
@@ -32,12 +32,10 @@ class Foo(AbstractFoo):
         
         super().__init__(name)
 
-foo = Foo('Bar')
-print(foo.name)
-
 print('\n------------------------------\n')
 
-
+foo = Foo('Bar')
+print(foo.name)
 
 print('\n------------------------------\n')
 
