cfe8b302468df0e185004f5114aafb7c9b33af5c
João Augusto Colasso Handocha <163477998+JoaoAugustoColassoHandocha@users.noreply.github.com>
2026-09-17 19:11:39 +0000

    Update Codigo_182.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_182.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_182.py
@@ -19,3 +19,13 @@ Uma classe abstrata em Python tem sua metaclasse sendo ABCMeta.
 
 '''
 
+import os
+
+print('\n------------------------------\n')
+
+
+
+print('\n------------------------------\n')
+
+input('Clique em qualquer tecla para continuar...')
+os.system('cls' if os.name == 'nt' else 'clear')
