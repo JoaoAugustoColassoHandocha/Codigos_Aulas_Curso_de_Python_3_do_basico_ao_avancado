5e070c55dce16df807dabf70abeec404b71503a6
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2026-09-23 17:18:58 -0300

    Update Codigo_184.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_184.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_184.py
@@ -25,5 +25,3 @@ print('\n------------------------------\n')
 
 print('\n------------------------------\n')
 
-input('Clique em qualquer tecla para continuar...')
-os.system('cls' if os.name == 'nt' else 'clear')
