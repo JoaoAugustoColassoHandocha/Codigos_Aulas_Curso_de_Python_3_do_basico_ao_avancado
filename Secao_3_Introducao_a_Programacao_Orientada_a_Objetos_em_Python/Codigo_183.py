222612b1224b26aadc2a50331c3f06dce34776ef
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2026-09-23 16:32:02 -0300

    Update Codigo_183.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_183.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_183.py
@@ -46,6 +46,5 @@ print('\n------------------------------\n')
 print(foo.name)
 
 print('\n------------------------------\n')
-
 input('Clique em qualquer tecla para continuar...')
 os.system('cls' if os.name == 'nt' else 'clear')
