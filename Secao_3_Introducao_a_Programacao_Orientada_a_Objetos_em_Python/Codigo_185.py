2865aac35b93fe1bb1f2c73778d5e9a735027ed9
JoaoAugustoColassoHandocha <joaoaugustocolassohandocha@gmail.com>
2026-09-24 17:03:04 -0300

    Update Codigo_185.py

--- Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_185.py
+++ Secao_3_Introducao_a_Programacao_Orientada_a_Objetos_em_Python/Codigo_185.py
@@ -3,16 +3,15 @@ Polimorfismo em Python Orientado a Objetos
 
 Polimorfismo é o princípio que permite que classes deridavas de uma mesma superclasse tenham métodos iguais (com mesma assinatura) mas comportamentos diferentes.
 
-# Assinatura do método = Mesmo nome e quantidade
-# de parâmetros (retorno não faz parte da assinatura)
-# Opinião + princípios que contam:
-# Assinatura do método: nome, parâmetros e retorno iguais
-# SO"L"ID
-# Princípio da substituição de liskov
-# Objetos de uma superclasse devem ser substituíveis
-# por objetos de uma subclasse sem quebrar a aplicação.
-# Sobrecarga de métodos (overload)  🐍 = ❌
-# Sobreposição de métodos (override) 🐍 = ✅
+Assinatura do método = Mesmo nome e quantidade de parâmetros (retorno não faz parte da assinatura)
+
+Opinião + princípios que contam:
+
+Assinatura do método: nome, parâmetros e retorno iguais SO"L"ID
+Princípio da substituição de liskov
+Objetos de uma superclasse devem ser substituíveis por objetos de uma subclasse sem quebrar a aplicação.
+Sobrecarga de métodos (overload)  🐍 = ❌
+Sobreposição de métodos (override) 🐍 = ✅
 
 '''
 
